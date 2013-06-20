95823c2f89f05f3aa692e6d4e77990b31ed41c80
mfleonawicz <mfleonawicz@alaska.edu>
2013-06-20 15:02:44 -0800

    About page updates

--- akcan_climate/external/about.R
+++ akcan_climate/external/about.R
@@ -1,11 +1,23 @@
 function(){
 	tabPanel("About",
-		p(style="text-align:justify",'Using an About tab from another app as a template. Edit all this later...'),
-		p(style="text-align:justify",'The key feature of the app is ...'),
-		p(style="text-align:justify",strong('Notes:'),' Geographic locations are represented by the names of the largest population centers...'),
+		p(style="text-align:justify",'This R Shiny web app is currently under development.
+		This release includes both historical and projected global climate model (GCM) output for temperature and precipitation, in the form of decadal means and totals, respectively.
+		There are five AR4/CMIP3 GCMs and three emissions scenarios for each.'),
+		p(style="text-align:justify",'The app currently has many features.
+		The upper sidebar panel is for selecting subsets of data, choosing units of measurement, and performing the subset action and creating a new plot.
+		The lower sidebar panel is for general plot formatting. Most options here will not auto-update the graphic in the main panel, but some may.
+		Generally the user must click the update button. The formatting options that appear in the main panel below the plot are ones that will force an auto-upate of the plot as soon as they are changed.
+		The app allows the user to look at many different kinds of subsets of the data in many ways. There is as much control over how the output graphic looks as there is in determining what data it graphs.
+		There are also buttons at the bottom of the main panel for downloading a csv file of the currently selected data and a pdf of the current plot.'),
+		p(style="text-align:justify",strong('Notes:'),
+		'The app uses a large but not complete subset of the same cities from Alaska and Western Canada which are included in <a/ "http://www.snap.uaf.edu/charts.phpSNAPs" target="_blank">Community Charts tool</a>.
+		There are somewhat fewer communities, including only those with populations greater than 2,500.
+		However, there is more versatility in terms of data sources, selection/subsetting options, user control, plotting,
+		and direct access to tabular data and graphic output in file format rather than strictly as browser viewing.'),
 		p(style="text-align:justify",'Positive values for directional wind velocity components indicate...'),
-		p(style="text-align:justify",strong('Suggestions:'),'You may download a graphic in pdf form...'),
-		p(style="text-align:justify",'For both the browser display and the pdf download...'),
+		p(style="text-align:justify",strong('Suggestions:'),'For the time being, this developmental version of the app should function very well. Stay tuned for updates.
+		There are a variety of features I am considering adding when I have time.
+		These will likely spill over onto additional main panel tabs, somewhere between the two currently called "Communities" and "About".'),
 		br(),
 
 		HTML('<div style="clear: left;"><img src="http://www.gravatar.com/avatar/52c27b8719a7543b4b343775183122ea.png" alt="" style="float: left; margin-right:5px" /></div>'),
@@ -21,15 +33,17 @@ function(){
 		div(class="row-fluid",
 			div(class="span4",strong('Related apps'),
 				p(HTML('<ul>'),
+					HTML('<li>'),a("Coastal Alaska extreme temperature and wind events", href="http://shiny.snap.uaf.edu/temp_wind_events/", target="_blank"),HTML('</li>'),
 					HTML('<li>'),a("Arctic Sea Ice Extents and Concentrations", href="http://shiny.snap.uaf.edu/sea_ice_coverage/", target="_blank"),HTML('</li>'),
 				HTML('</ul>')),
 				strong('Code'),
 				p('Source code available at',
-				a('GitHub', href="https://github.com/ua-snap/shiny-apps/tree/master/temp_wind_events/")),
+				a('GitHub', href="https://github.com/ua-snap/shiny-apps/tree/master/akcan_climate/")),
 				br()
 			),
 			div(class="span4", strong('Related blog posts'),
 				p(HTML('<ul>'),
+					HTML('<li>'),a("R Shiny app: Alaska and western Canada climate", href="http://blog.snap.uaf.edu/2013/05/20/r-shiny-web-app-extreme-events/", target="_blank"),HTML('</li>'),
 					HTML('<li>'),a("R Shiny web app: Coastal Alaska extreme temperature and wind events", href="http://blog.snap.uaf.edu/2013/05/20/r-shiny-web-app-extreme-events/", target="_blank"),HTML('</li>'),
 					HTML('<li>'),a("R Shiny web app: Arctic sea ice extents and concentrations", href="http://blog.snap.uaf.edu/2013/05/20/r-shiny-web-app-sea-ice/", target="_blank"),HTML('</li>'),
 				HTML('</ul>')),
@@ -42,10 +56,8 @@ function(){
 					HTML('<li>'),a('Built with the Shiny package', href="http://www.rstudio.com/shiny/", target="_blank"),HTML('</li>'),
 					HTML('<li>'),"Primary supporting R packages",HTML('</li>'),
 					HTML('<ul>'),
-						HTML('<li>'),a('mapproj', href="http://cran.r-project.org/web/packages/mapproj/", target="_blank"),HTML('</li>'),
-						HTML('<li>'),a('maps', href="http://cran.r-project.org/web/packages/maps/index.html", target="_blank"),HTML('</li>'),
-						HTML('<li>'),a('raster', href="http://cran.r-project.org/web/packages/raster/index.html", target="_blank"),HTML('</li>'),
-						HTML('<li>'),a('rasterVis', href="http://cran.r-project.org/web/packages/rasterVis/index.html", target="_blank"),HTML('</li>'),
+						HTML('<li>'),a('ggplot2', href="http://ggplot2.org", target="_blank"),HTML('</li>'),
+						HTML('<li>'),a('Hmisc', href="http://cran.r-project.org/web/packages/Hmisc/index.html", target="_blank"),HTML('</li>'),
 					HTML('<ul>'),
 				HTML('</ul>'))
 			)
--- gbm_example/external/about.R
+++ gbm_example/external/about.R
@@ -3,11 +3,12 @@ function(){
 		p(style="text-align:justify",'This R Shiny web app allows the user to perform stochastic gradient boosting on a simulated dataset using the R gbm package.
 		The simulated data come directly from the gbm help file. Currently, features are limited.'),
 		p(style="text-align:justify",strong('Notes:'),'Also, the app will throw an error if you try to perform cross-validation when gradient boosting, i.e., by setting the number of cross-validation folds > 1.
-		This metaparameter setting corresponds to the cv.folds argument to the gbm function. The help file example will run fine in a stand-alone R session,
+		This meta-parameter setting corresponds to the cv.folds argument to the gbm function. The help file example will run fine in a stand-alone R session,
 		but not when built into a Shiny app.
 		I speculate that this has something to do with the current version of the gbm package attempting to take advantage of the base R parallel package, but I have not been able to solve the problem.
 		If anyone else can figure it out, please let me know!'),
-		p(style="text-align:justify",'I would like to continue enhancing this app with many additional features and graphics. Stay tuned for other updates.'),
+		p(style="text-align:justify",'I would like to continue enhancing this app with many additional features and graphics, not to mention switching over from the simulated dataset to something more real and relevant.
+		Stay tuned for updates.'),
 		br(),
 
 		HTML('<div style="clear: left;"><img src="http://www.gravatar.com/avatar/52c27b8719a7543b4b343775183122ea.png" alt="" style="float: left; margin-right:5px" /></div>'),
@@ -32,7 +33,7 @@ function(){
 			),
 			div(class="span4", strong('Related blog posts'),
 				p(HTML('<ul>'),
-					HTML('<li>'),a("R Shiny app: Stochastic gradient boosting with gbm", href="http://blog.snap.uaf.edu/2013/05/20/r-shiny-app-stochastic-gradient-boosting-with-gbm/", target="_blank"),HTML('</li>'),
+					HTML('<li>'),a("R Shiny app: Stochastic gradient boosting with gbm", href="http://blog.snap.uaf.edu/2013/06/20/r-shiny-app-stochastic-gradient-boosting-with-gbm/", target="_blank"),HTML('</li>'),
 					HTML('<li>'),a("R sampling app version 4", href="http://blog.snap.uaf.edu/2013/05/20/r-sampling-app-version-4/", target="_blank"),HTML('</li>'),
 				HTML('</ul>')),
 				br()
