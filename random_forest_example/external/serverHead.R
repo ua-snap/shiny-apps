019e37e53a9e2cb802d8b2d9f58f2b0025c3dc03
mfleonawicz <mfleonawicz@alaska.edu>
2013-07-16 13:02:53 -0800

    updates

--- random_forest_example/external/serverHead.R
+++ random_forest_example/external/serverHead.R
@@ -3,7 +3,7 @@ pkgs <- c("randomForest","ggplot2","plyr","reshape2","gridExtra","gtable")
 pkgs <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
 if(length(pkgs)) install.packages(pkgs,repos="http://cran.cs.wwu.edu/")
 library(randomForest); library(ggplot2); library(plyr); library(reshape2); library(gridExtra); library(gtable); #library(parallel) # parallel experimental
-d <- read.csv("X:/Leonawicz/Projects/Project_flags/data/flagData.csv")
+d <- read.csv("external/flagData.csv")
 clrs <- c("#E69F00", "#56B4E9", "#8B4513", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#551A8B", "#999999")
 var.is.factor <- sapply(d[-1],is.factor)
 #num.vars <- which(!var.is.factor)
