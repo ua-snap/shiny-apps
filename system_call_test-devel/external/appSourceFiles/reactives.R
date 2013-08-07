12bcd5e3d3a18b097c0a0682affabb088c04c62d
mfleonawicz <mfleonawicz@alaska.edu>
2013-08-07 13:42:29 -0800

    commented out isolate call

--- system_call_test-devel/external/appSourceFiles/reactives.R
+++ system_call_test-devel/external/appSourceFiles/reactives.R
@@ -3,12 +3,12 @@ sysCall <- reactive({
 	x <- "Did not attempt to run system call"
 	if(!is.null(input$goButton)){
 		if(input$goButton==0) return(x)
-		isolate(
+		#isolate(
 			observe({
 				system("external/shell.txt external/script.R")
 				#system("Y:/Users/mfleonawicz/github/shiny-apps/system_call_test-devel/external/shell.txt Y:/Users/mfleonawicz/github/shiny-apps/system_call_test-devel/external/script.R")
 			})
-		)
+		#)
 		x <- paste("Attempted to run system call. wd:",getwd())
 	}
 	return(x)
