5a6881a6d216951ec6d71a88156d6d0e2ba77d35
mfleonawicz <mfleonawicz@alaska.edu>
2013-08-07 13:54:28 -0800

    path change

--- system_call_test-devel/external/appSourceFiles/reactives.R
+++ system_call_test-devel/external/appSourceFiles/reactives.R
@@ -5,8 +5,7 @@ sysCall <- reactive({
 		if(input$goButton==0) return(x)
 		#isolate(
 			observe({
-				system(paste0(getwd(),"/external/shell.txt ",getwd(),"/external/script.R",sep=""))
-				#system("Y:/Users/mfleonawicz/github/shiny-apps/system_call_test-devel/external/shell.txt Y:/Users/mfleonawicz/github/shiny-apps/system_call_test-devel/external/script.R")
+				system(paste0(getwd(),"/external/shell.txt ",getwd(),"/external/script.R"))
 			})
 		#)
 		x <- paste("Attempted to run system call. wd:",getwd())
