f66040bcb9d411f152a9d5e8676b0367be410f03
mfleonawicz <mfleonawicz@alaska.edu>
2013-08-07 13:34:28 -0800

    added observer

--- system_call_test-devel/external/appSourceFiles/reactives.R
+++ system_call_test-devel/external/appSourceFiles/reactives.R
@@ -4,8 +4,10 @@ sysCall <- reactive({
 	if(!is.null(input$goButton)){
 		if(input$goButton==0) return(x)
 		isolate(
-			system(paste0(getwd(),"/external/shell.txt ",getwd(),"/external/script.R",sep=""))
-			#system("Y:/Users/mfleonawicz/github/shiny-apps/system_call_test-devel/external/shell.txt Y:/Users/mfleonawicz/github/shiny-apps/system_call_test-devel/external/script.R")
+			observe({
+				system(paste0(getwd(),"/external/shell.txt ",getwd(),"/external/script.R",sep=""))
+				#system("Y:/Users/mfleonawicz/github/shiny-apps/system_call_test-devel/external/shell.txt Y:/Users/mfleonawicz/github/shiny-apps/system_call_test-devel/external/script.R")
+			})
 		)
 		x <- paste("Attempted to run system call. wd:",getwd())
 	}
