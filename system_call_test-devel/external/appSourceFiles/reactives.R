9358d3e932c7f14f92a5ed551ff703704086eade
mfleonawicz <mfleonawicz@alaska.edu>
2013-08-23 13:02:20 -0800

    observer testing

--- system_call_test-devel/external/appSourceFiles/reactives.R
+++ system_call_test-devel/external/appSourceFiles/reactives.R
@@ -3,12 +3,12 @@ sysCall <- reactive({
 	x <- "Did not attempt to run system call"
 	if(!is.null(input$goButton)){
 		if(input$goButton==0) return(x)
-		isolate(
-			system("touch /tmp/file.tmp"),
+		isolate({
+			system("touch /tmp/file.tmp")
 			observe({
 				system("/tmp/shell.txt /tmp/script.R") #, wait=FALSE, show.output.on.console=FALSE)
 			})
-		)
+		})
 		x <- paste("Attempted to run system call. wd:",paste0(getwd(),"/shell.txt ",getwd(),"/script.R"))
 	}
 	return(x)
