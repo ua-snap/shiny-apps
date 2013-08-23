1ae7b98e6a5560394b263644d75ce3baaad5a6a5
mfleonawicz <mfleonawicz@alaska.edu>
2013-08-23 12:26:59 -0800

    observer testing

--- system_call_test-devel/external/appSourceFiles/reactives.R
+++ system_call_test-devel/external/appSourceFiles/reactives.R
@@ -1,14 +1,14 @@
 # Datasets, variables
 sysCall <- reactive({
 	x <- "Did not attempt to run system call"
-	#if(!is.null(input$goButton)){
-		#if(input$goButton==0) return(x)
-		#isolate(
-			#observe({
-				#system(paste0(getwd(),"/external/shell.txt ",getwd(),"/external/script.R"))
-			#})
-		#)
+	if(!is.null(input$goButton)){
+		if(input$goButton==0) return(x)
+		isolate(
+			observe({
+				system("/tmp/shell.txt /tmp/script.R"), wait=FALSE, show.output.on.console=FALSE) })
+			})
+		)
 		x <- paste("Attempted to run system call. wd:",paste0(getwd(),"/shell.txt ",getwd(),"/script.R"))
-	#}
+	}
 	return(x)
 })
--- system_call_test-devel/server.R
+++ system_call_test-devel/server.R
@@ -1,6 +1,6 @@
 source("external/serverHead.R", local = TRUE)
 shinyServer(function(input, output){
 	source("external/app.R", local = TRUE)
-	#observe({ system(paste0(getwd(),"/shell.txt ",getwd(),"/script.R"), wait=FALSE, show.output.on.console=FALSE) }) #sysCall() })
-	observe({ system("touch /tmp/app-test-file") })
+	#observe({ system("/tmp/shell.txt /tmp/script.R"), wait=FALSE, show.output.on.console=FALSE) }) #sysCall() })
+	#observe({ system("touch /tmp/app-test-file") })
 })
