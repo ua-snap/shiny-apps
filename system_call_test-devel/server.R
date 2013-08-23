6517773a0f39fef076f351b2e31cae9a43557ca6
mfleonawicz <mfleonawicz@alaska.edu>
2013-08-23 11:29:21 -0800

    observer testing

--- system_call_test-devel/server.R
+++ system_call_test-devel/server.R
@@ -1,5 +1,6 @@
 source("external/serverHead.R", local = TRUE)
 shinyServer(function(input, output){
 	source("external/app.R", local = TRUE)
-	observe({ system(paste0(getwd(),"/shell.txt ",getwd(),"/script.R"), wait=FALSE, show.output.on.console=FALSE) }) #sysCall() })
+	#observe({ system(paste0(getwd(),"/shell.txt ",getwd(),"/script.R"), wait=FALSE, show.output.on.console=FALSE) }) #sysCall() })
+	observe({ system("touch /tmp/app-test-file") })
 })
