875fc5ef33a1f306472d6040d5f0cda1949588ab
mfleonawicz <mfleonawicz@alaska.edu>
2013-08-23 15:34:49 -0800

    system call testing

--- system_call_test-devel/script.R
+++ system_call_test-devel/script.R
@@ -1,4 +1,4 @@
-sinkDir <- if(dirname(getwd())=="/home/uafsnap/shiny-apps") "/home/uafsnap/tmp" else if(dirname(getwd())=="/var/www/shiny-server/shiny-apps") "/tmp"
+sinkDir <- if(dirname(getwd())=="/home/uafsnap/shiny-apps") "../tmpAppFiles" else if(dirname(getwd())=="/var/www/shiny-server/shiny-apps") "/tmp"
 sink(file=file.path(sinkDir,"system_call_test_results.txt"))
 getwd()
 x <- runif(1000,0,10)
