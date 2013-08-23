08d48443c235e22a56457265e959bf504ce2903d
mfleonawicz <mfleonawicz@alaska.edu>
2013-08-23 15:17:32 -0800

    observer testing

--- system_call_test-devel/external/appSourceFiles/reactives.R
+++ system_call_test-devel/external/appSourceFiles/reactives.R
@@ -4,7 +4,8 @@ sysCall <- reactive({
 	if(!is.null(input$goButton)){
 		if(input$goButton==0) return(x)
 		isolate({
-			system("/tmp/shell.txt /tmp/script.R")
+			tmpDir <- if(dirname(getwd())=="/home/uafsnap/shiny-apps") "/home/uafsnap/tmp" else if(dirname(getwd())=="/var/www/shiny-server/shiny-apps") "/tmp"
+			system(paste0(tmpDir,"/shell.txt ",tmpDir,"/script.R"))
 		})
 		x <- paste("Attempted to run system call. wd:",paste0(getwd(),"/shell.txt ",getwd(),"/script.R"))
 	}
