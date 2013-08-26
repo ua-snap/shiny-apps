1f889f556d22f5fbfc4b7d05fb1307eb1c661bac
mfleonawicz <mfleonawicz@alaska.edu>
2013-08-26 12:41:40 -0800

    edited header logo

--- fairbanks_daily_precipitation/external/header.R
+++ fairbanks_daily_precipitation/external/header.R
@@ -2,9 +2,6 @@ headerPanel_2(
 	HTML(
 		'<div id="stats_header">
 		Customizable charts: Fairbanks, Alaska daily precipitation
-		<a href="http://accap.uaf.edu" target="_blank">
-		<img id="stats_logo" align="right" alt="ACCAP Logo" src="./img/accap_sidebyside_black-bkgrnd.png" />
-		</a>
 		<a href="http://snap.uaf.edu" target="_blank">
 		<img id="stats_logo" align="right" alt="SNAP Logo" src="./img/snap_sidebyside_black-bkgrnd.png" />
 		</a>
