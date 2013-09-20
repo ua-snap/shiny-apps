d81e66d2d2c1f96d4c8480529bd0c49b68c340de
mfleonawicz <mfleonawicz@alaska.edu>
2013-09-19 16:28:31 -0800

    altered initial display

--- ak_daily_precipitation/external/appSourceFiles/io.mainPanel.tp1.R
+++ ak_daily_precipitation/external/appSourceFiles/io.mainPanel.tp1.R
@@ -2,6 +2,6 @@
 output$tp.dailyTitle <- renderUI({
 	input$genPlotButton
 	isolate({
-		if(!is.null(d())) h4(paste(input$loc,"daily precipitation"))
+		if(is.null(d())) h4("New plots may take several seconds to generate") else h4(paste(input$loc,"daily precipitation"))
 	})
 })
