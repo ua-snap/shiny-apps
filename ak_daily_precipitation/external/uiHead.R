0abf4eae48c28e7d90030702d9afe16211957d8f
mfleonawicz <mfleonawicz@alaska.edu>
2014-01-10 16:22:05 -0900

    updates

--- ak_daily_precipitation/external/uiHead.R
+++ ak_daily_precipitation/external/uiHead.R
@@ -1,21 +1,20 @@
 helpPopup <- function(title, content,
-                      placement=c('right', 'top', 'left', 'bottom'),
-                      trigger=c('focus', 'hover', 'click', 'manual')) {
-  tagList(
-    singleton(
-      tags$head(
-        tags$script("$(function() { $(\"[data-toggle='popover']\").popover(); })")
-      )
-    ),
-    tags$a(
-      href = "#", `data-toggle` = "popover", #class = "btn btn-mini",
-      title = title, `data-content` = content, `data-animation` = TRUE,
-      `data-placement` = match.arg(placement, several.ok=TRUE)[1],
-      `data-trigger` = match.arg(trigger, several.ok=TRUE)[1],
-      
-      tags$i(class="icon-question-sign")
-    )
-  )
+placement=c('right', 'top', 'left', 'bottom'),
+trigger=c('click', 'hover', 'focus', 'manual')) {
+tagList(
+singleton(
+tags$head(
+tags$script("$(function() { $(\"[data-toggle='popover']\").popover(); })")
+)
+),
+tags$a(
+href = "#", class = "btn btn-mini", `data-toggle` = "popover",
+title = title, `data-content` = content, `data-animation` = TRUE,
+`data-placement` = match.arg(placement, several.ok=TRUE)[1],
+`data-trigger` = match.arg(trigger, several.ok=TRUE)[1],
+tags$i(class="icon-question-sign")
+)
+)
 }
 
 source("external/appSourceFiles/help.popups.R",local=T)
