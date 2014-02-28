86d7feb194e1f28814f784e99675201202049b1f
mfleonawicz <mfleonawicz@alaska.edu>
2014-02-28 12:01:56 -0900

    temporary changes for debugging performance on Eris

--- system_call_test-devel/external/main.R
+++ system_call_test-devel/external/main.R
@@ -1,7 +1,7 @@
 mainPanel(
 	tabsetPanel(
 		tabPanel("Home", 
-			div(verbatimTextOutput("Obs_UpdateFiles")style="height: 100px;"), h1(textOutput("WelcomeTitle")), h3(textOutput("WelcomeSubtitle")), value="home"),
+			div(verbatimTextOutput("Obs_UpdateFiles"), style="height: 100px;"), h1(textOutput("WelcomeTitle")), h3(textOutput("WelcomeSubtitle")), value="home"),
 		tabPanel("View FIF", 
 			div(verbatimTextOutput("FIF_Lines"), style="height: 650px;"), value="fif"),
 		tabPanelAbout(),
