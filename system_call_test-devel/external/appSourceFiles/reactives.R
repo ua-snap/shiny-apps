12750a2589733ebbf6aa557aac5b84eeccbbb1b3
mfleonawicz <mfleonawicz@alaska.edu>
2014-02-28 10:28:28 -0900

    temporary changes for debugging performance on Eris

--- system_call_test-devel/external/appSourceFiles/reactives.R
+++ system_call_test-devel/external/appSourceFiles/reactives.R
@@ -39,48 +39,74 @@ fif_lines <- reactive({
 })
 
 Obs_updateFiles <- observe({
-	fire.sensitivity.sub <- as.character(as.numeric(input$FireSensitivity))
-	ignition.factor.sub <- as.character(as.numeric(input$IgnitionFactor))
-	if(!is.na(fire.sensitivity.sub) & !is.na(ignition.factor.sub)){
-		#white spaces below are just to make the file prettier on visual inspection in an editor
-		fire.sensitivity.sub.fif <- paste0("Fire.Sensitivity                         = {", fire.sensitivity.sub, "}                                         ;")
-		ignition.factor.sub.fif <- paste0("Fire.IgnitionFactor                      = {",ignition.factor.sub,"}                                          ;")
-		for(i in fif_current()){
-			x <- readLines(i)
-			x <- gsub( "^Fire\\.Sensitivity\\s+=\\s+\\{\\d*\\.?\\d*\\}\\s+;", fire.sensitivity.sub.fif, x)
-			x <- gsub( "^Fire\\.IgnitionFactor\\s+=\\s+\\{\\d*\\.?\\d*\\}\\s+;", ignition.factor.sub.fif, x)
-			cat(x, file=i, sep="\n")
-		}
-		if(input$update_fif_defaults){
-			for(i in defaults_file){
+	if(!is.null(input$FireSensitivity) & !is.null(input$IgnitionFactor)){
+		fire.sensitivity.sub <- as.character(as.numeric(input$FireSensitivity))
+		ignition.factor.sub <- as.character(as.numeric(input$IgnitionFactor))
+		if(!is.na(fire.sensitivity.sub) & !is.na(ignition.factor.sub)){
+			#white spaces below are just to make the file prettier on visual inspection in an editor
+			fire.sensitivity.sub.fif <- paste0("Fire.Sensitivity                         = {", fire.sensitivity.sub, "}                                         ;")
+			ignition.factor.sub.fif <- paste0("Fire.IgnitionFactor                      = {",ignition.factor.sub,"}                                          ;")
+			for(i in fif_current()){
 				x <- readLines(i)
-				x <- gsub( "^default_Fire\\.Sensitivity=\\d*\\.?\\d*", paste0("default_Fire.Sensitivity=",as.numeric(input$FireSensitivity)), x)
-				x <- gsub( "^default_Fire\\.IgnitionFactor=\\d*\\.?\\d*", paste0("default_Fire.IgnitionFactor=",as.numeric(input$IgnitionFactor)), x)
+				x <- gsub( "^Fire\\.Sensitivity\\s+=\\s+\\{\\d*\\.?\\d*\\}\\s+;", fire.sensitivity.sub.fif, x)
+				x <- gsub( "^Fire\\.IgnitionFactor\\s+=\\s+\\{\\d*\\.?\\d*\\}\\s+;", ignition.factor.sub.fif, x)
 				cat(x, file=i, sep="\n")
 			}
+			if(input$update_fif_defaults){
+				for(i in defaults_file){
+					x <- readLines(i)
+					x <- gsub( "^default_Fire\\.Sensitivity=\\d*\\.?\\d*", paste0("default_Fire.Sensitivity=",as.numeric(input$FireSensitivity)), x)
+					x <- gsub( "^default_Fire\\.IgnitionFactor=\\d*\\.?\\d*", paste0("default_Fire.IgnitionFactor=",as.numeric(input$IgnitionFactor)), x)
+					cat(x, file=i, sep="\n")
+				}
+			}
+			
+			# system calls begin here
+			# setup
+			user <- "sudo -u shiny"
+			server <- "atlas.snap.uaf.edu"
+			mainDir <- "~/shiny/mfleonawicz"
+			outDir <- paste0(mainDir,"/Runs_Noatak/Ignit_",ignition.factor.sub,"_Sens",fire.sensitivity.sub,"_complexGBMs")
+			
+			# Create Alfresco run-specific output directory and give my user read/write permissions
+			system(paste(user, "ssh", server, "mkdir", outDir))
+			system(paste(user, "ssh", server, "chmod 2775", outDir))
+			
+			system(paste("ssh", server, "cp", file.path(mainDir,"RunAlfresco_Noatak.slurm"), file.path(outDir,"RunAlfresco_Noatak.slurm"),"/"))
+			system(paste("ssh", server, "cp", file.path(mainDir,"CompileData_Noatak.slurm"), file.path(outDir,"CompileData_Noatak.slurm"),"/"))
+			system(paste0("sudo -u shiny scp ", input$fif_files, " ", server, ":", file.path(outDir,input$fif_files),"/"))
+			
+			exec <- "sbatch"
+			file <- "RunAlfresco_Noatak.slurm"
+			#args <- "~/script.R 2 n=100 mean=10 sd=2"
+			system(paste(user,"ssh",server,exec,file.path(outDir,file)))#,args))
 		}
-		
-		# system calls begin here
-		# setup
-		user <- "sudo -u shiny"
-		server <- "atlas.snap.uaf.edu"
-		mainDir <- "~/shiny/mfleonawicz"
-		outDir <- paste0(mainDir,"/Runs_Noatak/Ignit_",ignition.factor.sub,"_Sens",fire.sensitivity.sub,"_complexGBMs")
-		
-		# Create Alfresco run-specific output directory and give my user read/write permissions
-		system(paste(user, "ssh", server, "mkdir", outDir))
-		system(paste(user, "ssh", server, "chmod 2775", outDir))
-		
-		system(paste("ssh", server, "cp", file.path(mainDir,"RunAlfresco_Noatak.slurm"), file.path(outDir,"RunAlfresco_Noatak.slurm"),"/"))
-		system(paste("ssh", server, "cp", file.path(mainDir,"CompileData_Noatak.slurm"), file.path(outDir,"CompileData_Noatak.slurm"),"/"))
-		system(paste0("sudo -u shiny scp ", input$fif_files, " ", server, ":", file.path(outDir,input$fif_files),"/"))
-		
-		exec <- "sbatch"
-		file <- "RunAlfresco_Noatak.slurm"
-		#args <- "~/script.R 2 n=100 mean=10 sd=2"
-		system(paste(user,"ssh",server,exec,file.path(outDir,file)))#,args))
 	}
-},
-suspended=T)
+}#, suspended=T
+)
 
-Obs_updateFiles_resume <- observe({ if(!is.null(input$goButton_fif)) if(input$goButton_fif == 0) Obs_updateFiles$resume() })
+#Obs_updateFiles_resume <- observe({ if(!is.null(input$goButton_fif)) if(input$goButton_fif == 0) Obs_updateFiles$resume() })
+
+runAlf <- reactive({
+	if(!is.null(input$goButton_fif)){
+		if(input$goButton_fif > 0){
+			isolate({
+				fire.sensitivity.sub <- as.character(as.numeric(input$FireSensitivity))
+				ignition.factor.sub <- as.character(as.numeric(input$IgnitionFactor))
+				if(!is.na(fire.sensitivity.sub) & !is.na(ignition.factor.sub)){
+					user <- "sudo -u shiny"
+					server <- "atlas.snap.uaf.edu"
+					mainDir <- "~/shiny/mfleonawicz"
+					outDir <- paste0(mainDir,"/Runs_Noatak/Ignit_",ignition.factor.sub,"_Sens",fire.sensitivity.sub,"_complexGBMs")
+					exec <- "sbatch"
+					file <- "RunAlfresco_Noatak.slurm"
+					#args <- "~/script.R 2 n=100 mean=10 sd=2"
+					system(paste(user,"ssh",server,exec,file.path(outDir,file)))#,args))
+				}
+			})
+		}
+	}
+	return(input$goButton_fif)
+})
+
+	
