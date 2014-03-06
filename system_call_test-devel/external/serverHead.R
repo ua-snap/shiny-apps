2da1fb54fc9def3f1ae03915d357f1b2ab8a6570
mfleonawicz <mfleonawicz@alaska.edu>
2014-03-06 14:30:28 -0900

    code for acceptin/using email addresses input by users

--- system_call_test-devel/external/serverHead.R
+++ system_call_test-devel/external/serverHead.R
@@ -1,9 +1,8 @@
 library(shiny)
-#pkgs <- c("randomForest","ggplot2","plyr","reshape2","gridExtra","gtable")
-#pkgs <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
-#if(length(pkgs)) install.packages(pkgs,repos="http://cran.cs.wwu.edu/")
-#library(randomForest); library(ggplot2); library(plyr); library(reshape2); library(gridExtra); library(gtable); library(parallel) # parallel experimental
-datasets <- c("iris","cars")
+pkgs <- c("assertive")
+pkgs <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
+if(length(pkgs)) install.packages(pkgs,repos="http://cran.cs.wwu.edu/")
+library(assertive)
 defaults_file <- "external/appSourceFiles/alfresco_fif_defaults.txt"
 source(defaults_file, local=T)
 fif_files <- list.files(pattern=".fif$")
