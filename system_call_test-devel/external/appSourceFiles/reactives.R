6f8abca47832c0d57cc35277b6f1eeb100b36c96
mfleonawicz <mfleonawicz@alaska.edu>
2014-03-11 13:34:23 -0800

    code for accepting/using email addresses input by users

--- system_call_test-devel/external/appSourceFiles/reactives.R
+++ system_call_test-devel/external/appSourceFiles/reactives.R
@@ -104,7 +104,7 @@ Obs_updateFiles <- reactive({
 			system(paste("ssh", server, "cp", file.path(mainDir,"CompileData_Noatak.slurm"), file.path(outDir,"CompileData_Noatak.slurm")))
 			system(paste0("scp ", input$fif_files, " ", server, ":", file.path(outDir,input$fif_files)))
 			slurm_arguments <- paste("-D", outDir)
-			arguments <- outDir
+			arguments <- paste(c(outDir, all_email_addresses()), collapse=" ")
 			system(paste(user,"ssh",server,exec, slurm_arguments, file.path(outDir,slurmfile), arguments))
 		}
 		x <- "Alfresco job started on Atlas" #paste("Can read, but can't update local files:", fif_current(), "and", defaults_file, ".", "Can't create directory on atlas:", outDir)
