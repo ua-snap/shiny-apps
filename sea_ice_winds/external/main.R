7940af1a6cf560f13b8f63288d3848ec85c4073a
mfleonawicz <mfleonawicz@alaska.edu>
2014-03-25 12:55:44 -0800

    Aesthetic changes

--- sea_ice_winds/external/main.R
+++ sea_ice_winds/external/main.R
@@ -6,8 +6,8 @@ mainPanel(
 			fluidRow(
 				column(3, uiOutput("tp.annstyle")),
 				column(3, uiOutput("tp.decstyle")),
-				column(3,downloadButton("dl_plotByYear","Download annual graphic")),
-				column(3, downloadButton("dl_plotByDecade","Download decadal graphic"))
+				column(3, br(), downloadButton("dl_plotByYear","Download annual graphic")),
+				column(3, br(), downloadButton("dl_plotByDecade","Download decadal graphic"))
 			), value="ts"),
 		tabPanelAbout(),
 		id="tsp"
