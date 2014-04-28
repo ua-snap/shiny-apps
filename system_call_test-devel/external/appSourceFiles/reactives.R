ef67c53bfc9e69b0c9dfeba84f52e9351e1b18bd
mfleonawicz <mfleonawicz@alaska.edu>
2014-04-28 13:43:41 -0800

    Add fire return period post-processing capability.

--- system_call_test-devel/external/appSourceFiles/reactives.R
+++ system_call_test-devel/external/appSourceFiles/reactives.R
@@ -122,10 +122,10 @@ Obs_updateFiles <- reactive({
 				
 				slurm_arguments <- paste("-D", outDir)
 				buffers <- paste(1000*as.numeric(unlist(strsplit(input$frp_buffers,","))), collapse=",")
-				buffers <- paste0("c(", buffers, ")", collapse="")
-				frp_arguments <- c(paste0("'buffers=", buffers, "'"))
+				buffers <- paste0("c\\(", buffers, "\\)", collapse="")
+				frp_arguments <- c(paste0("pts=", input$frp_pts), paste0("'buffers=", buffers, "'"))
 				if(input$skipAlf) postprocOnly <- 0 else postprocOnly <- 1
-				arguments <- paste(c(mainDir, outDir, relDir, paste(all_email_addresses(), collapse=","), alf.domain, input$fif_files, postprocOnly, frp_arguments[1]), collapse=" ")
+				arguments <- paste(c(mainDir, outDir, relDir, paste(all_email_addresses(), collapse=","), alf.domain, input$fif_files, postprocOnly, frp_arguments[1], frp_arguments[2]), collapse=" ")
 				sbatch_string <- paste(user,"ssh",server,exec, slurm_arguments, file.path(outDir,slurmfile), arguments)
 				system(sbatch_string)
 				x <- paste("Alfresco job started on Atlas:\n",gsub(" ", " \n", sbatch_string))
