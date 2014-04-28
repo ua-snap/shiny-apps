6c19cd4b429860e7d205f3c51276a37adc1786c8
mfleonawicz <mfleonawicz@alaska.edu>
2014-04-28 10:31:21 -0800

    Add fire return period post-processing capability.

--- system_call_test-devel/external/appSourceFiles/reactives.R
+++ system_call_test-devel/external/appSourceFiles/reactives.R
@@ -125,7 +125,7 @@ Obs_updateFiles <- reactive({
 				buffers <- paste0("c(", buffers, ")", collapse="")
 				frp_arguments <- paste0("'pts=", input$frp_pts, " ", "buffers=", buffers, "'", collapse="")
 				if(input$skipAlf) postprocOnly <- 0 else postprocOnly <- 1
-				arguments <- paste(c(mainDir, outDir, relDir, paste(all_email_addresses(), collapse=","), alf.domain, input$fif_files, postprocOnly, frp_arguments), collapse=" ")
+				arguments <- paste(c(mainDir, outDir, relDir, paste(all_email_addresses(), collapse=","), alf.domain, input$fif_files, frp_arguments), collapse=" ")
 				sbatch_string <- paste(user,"ssh",server,exec, slurm_arguments, file.path(outDir,slurmfile), arguments)
 				system(sbatch_string)
 				x <- paste("Alfresco job started on Atlas:\n",gsub(" ", " \n", sbatch_string))
