3f94ae4837b730e4999890dc3ef09771b8bbc8fc
mfleonawicz <mfleonawicz@alaska.edu>
2014-05-01 15:31:15 -0800

    Add global.R.

---
+++ system_call_test-devel/global.R
@@ -0,0 +1,16 @@
+library(shinyAce)
+
+R_files <- paste0(c(
+	"global", "ui", "server",
+	file.path("external", c("app", "sidebar", "main", "about")), # cannot include "header" if it contains Google Analytics tracking code
+	file.path("external/appSourceFiles", c(paste0("io.sidebar.wp", 1:2), "reactives"))
+	), ".R")
+
+showCode <- function(file, ht="600px"){
+	list(
+		h4(HTML(basename(file))),
+		aceEditor(gsub('\\.', '', basename(file)), value=paste(readLines(file), collapse='\n'), mode='r', height=ht, readOnly=TRUE)
+	)
+}
+
+for(i in 1:length(R_files)) assign(paste0("show_", gsub("\\.R", "R", basename(R_files[i]))), showCode(R_files[i]))
