6fe57425e2eb09fd0e84497a17bbaa1a702a9387
mfleonawicz <mfleonawicz@alaska.edu>
2014-04-30 16:41:27 -0800

    RGL

--- plot3D/server.R
+++ plot3D/server.R
@@ -2,6 +2,7 @@ library(shiny)
 pkgs <- c("plot3D","rgl","shinyRGL")
 pkgs <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
 if(length(pkgs)) install.packages(pkgs,repos="http://cran.cs.wwu.edu/")
+options(rgl.useNULL=TRUE)
 library(rgl); library(plot3D); library(shinyRGL)
 
 load("external/data.RData",envir=.GlobalEnv)
