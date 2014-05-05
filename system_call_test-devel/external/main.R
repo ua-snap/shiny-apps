f8aaef29e7cd61a8e689c692c9deb2bc8e7e7d30
mfleonawicz <mfleonawicz@alaska.edu>
2014-05-05 14:47:11 -0800

    shiny-server version debug/test.

--- system_call_test-devel/external/main.R
+++ system_call_test-devel/external/main.R
@@ -8,13 +8,13 @@ mainPanel( # testing
 		tabPanel("R Code",
 			#uiOutput("codeTab"), # This single line preferable to the below lines
 			# Cannot use the above style due to known bug in shinyAce package
-			conditionalPanel(condition="input.nlp==='nlp_aboutR'", show_aboutR),
-			conditionalPanel(condition="input.nlp==='nlp_appR'", show_appR),
-			conditionalPanel(condition="input.nlp==='nlp_globalR'", show_globalR),
+			#conditionalPanel(condition="input.nlp==='nlp_aboutR'", show_aboutR),
+			#conditionalPanel(condition="input.nlp==='nlp_appR'", show_appR),
+			#conditionalPanel(condition="input.nlp==='nlp_globalR'", show_globalR),
 			# cannot include "header" if it contains Google Analytics tracking code
 			#conditionalPanel(condition="input.nlp==='nlp_headerR'", show_headerR),
-			conditionalPanel(condition="input.nlp==='nlp_io.sidebar.wp1R'", show_io.sidebar.wp1R),
-			conditionalPanel(condition="input.nlp==='nlp_io.sidebar.wp2R'", show_io.sidebar.wp2R),
+			#conditionalPanel(condition="input.nlp==='nlp_io.sidebar.wp1R'", show_io.sidebar.wp1R),
+			#conditionalPanel(condition="input.nlp==='nlp_io.sidebar.wp2R'", show_io.sidebar.wp2R),
 			#conditionalPanel(condition="input.nlp==='nlp_mainR'", show_mainR),
 			conditionalPanel(condition="input.nlp==='nlp_reactivesR'", show_reactivesR),
 			conditionalPanel(condition="input.nlp==='nlp_serverR'", show_serverR),
