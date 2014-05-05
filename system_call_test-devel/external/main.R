f91ff76a4471694ac8835def2f82e159569db96e
mfleonawicz <mfleonawicz@alaska.edu>
2014-05-05 14:56:57 -0800

    shiny-server version debug/test.

--- system_call_test-devel/external/main.R
+++ system_call_test-devel/external/main.R
@@ -10,9 +10,9 @@ mainPanel( # testing
 			# Cannot use the above style due to known bug in shinyAce package
 			#conditionalPanel(condition="input.nlp==='nlp_aboutR'", show_aboutR),
 			#conditionalPanel(condition="input.nlp==='nlp_appR'", show_appR),
-			#conditionalPanel(condition="input.nlp==='nlp_globalR'", show_globalR),
+			conditionalPanel(condition="input.nlp==='nlp_globalR'", show_globalR),
 			# cannot include "header" if it contains Google Analytics tracking code
-			conditionalPanel(condition="input.nlp==='nlp_headerR'", show_headerR),
+			#conditionalPanel(condition="input.nlp==='nlp_headerR'", show_headerR),
 			conditionalPanel(condition="input.nlp==='nlp_io.sidebar.wp1R'", show_io.sidebar.wp1R),
 			#conditionalPanel(condition="input.nlp==='nlp_io.sidebar.wp2R'", show_io.sidebar.wp2R),
 			conditionalPanel(condition="input.nlp==='nlp_mainR'", show_mainR),
