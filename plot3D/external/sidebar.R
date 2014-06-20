95ddccbd7b519adf005e6ed65806265f4f2012a1
mfleonawicz <mfleonawicz@alaska.edu>
2014-06-20 14:13:58 -0800

    Updated widget displays based on Shiny version 0.10.0

--- plot3D/external/sidebar.R
+++ plot3D/external/sidebar.R
@@ -13,7 +13,7 @@ column(4,
 			),
 			conditionalPanel(condition="input.showWP1",
 				div(class="row-fluid",
-					div(class="span12",selectInput("dataset", "Select data:", choices=dataset.names, selected=dataset.names[1]))#,
+					div(class="span12",selectInput("dataset", "Select data:", choices=dataset.names, selected=dataset.names[1], width="100%"))#,
 					#div(class="span6",uiOutput("plottype"))
 				)
 			)
@@ -27,12 +27,12 @@ column(4,
 					div(class="span6",
 						selectInput("contour.col","Contour line color:",
 							c("Black","White","Gray","Orange","Blue","Red","green",
-								"R:Topo","R:Terrain","R:Heat","R:Cyan-Magenta","purple-yellow"),"R:Topo"),
-						sliderInput("contour.lwd","Line width:",1,5,1,1),
-						selectInput("contour.asp", "Aspect ratio:", c("Original scale","Rescale to square"),"Original scale")),
+								"R:Topo","R:Terrain","R:Heat","R:Cyan-Magenta","purple-yellow"),"R:Topo", width="100%"),
+						sliderInput("contour.lwd","Line width:",1,5,1,1, width="100%"),
+						selectInput("contour.asp", "Aspect ratio:", c("Original scale","Rescale to square"),"Original scale", width="100%")),
 					div(class="span6",
-						selectInput("contour.nlevels","Approx. levels:",c(5,10,20,40),10),
-						sliderInput("contour.labcex","Contour label size:",0.5,2,1,0.1),
+						selectInput("contour.nlevels","Approx. levels:",c(5,10,20,40),10, width="100%"),
+						sliderInput("contour.labcex","Contour label size:",0.5,2,1,0.1, width="100%"),
 						checkboxInput("contour.drawlabels","Draw labels",TRUE))
 				)
 			)
@@ -45,28 +45,28 @@ column(4,
 				div(class="row-fluid",
 					div(class="span6", 
 						selectInput("image.col","Image colors:",
-							c("R:Topo","R:Terrain","R:Heat","R:Cyan-Magenta","black-white","purple-yellow"),"R:Topo")),
-					div(class="span6", sliderInput("image.nlevels","Number of breaks:",6,30,16,2))
+							c("R:Topo","R:Terrain","R:Heat","R:Cyan-Magenta","black-white","purple-yellow"),"R:Topo", width="100%")),
+					div(class="span6", sliderInput("image.nlevels","Number of breaks:",6,30,16,2, width="100%"))
 				),
 				div(class="row-fluid",
-					div(class="span6", selectInput("resfac", "Increase resolution by factor:", 1:4,1)),
+					div(class="span6", selectInput("resfac", "Increase resolution by factor:", 1:4,1, width="100%")),
 					div(class="span6",
 						checkboxInput("rasterImage","rasterImage() smoothing"),
 						checkboxInput("contour","Add contour lines:"))
 				),
 				div(class="row-fluid",
 					div(class="span6", 
-						selectInput("facets","Color cells or borders:", choices=c("Grid cells","Borders"), selected="Grid cells")),
-					div(class="span6", selectInput("border", "Cell border color:", c("","White","Black"),""))
+						selectInput("facets","Color cells or borders:", choices=c("Grid cells","Borders"), selected="Grid cells", width="100%")),
+					div(class="span6", selectInput("border", "Cell border color:", c("","White","Black"),"", width="100%"))
 				),
 				div(class="row-fluid",
-					div(class="span6", selectInput("lighting", "Lighting:", c("None","Default"),"None")),
-					div(class="span6", sliderInput("shade","Shading:",0,1,0.5,0.05))
+					div(class="span6", selectInput("lighting", "Lighting:", c("None","Default"),"None", width="100%")),
+					div(class="span6", sliderInput("shade","Shading:",0,1,0.5,0.05, width="100%"))
 				),
 				div(class="row-fluid",
 					div(class="span6", 
-						selectInput("image.asp", "Aspect ratio:", c("Original scale","Rescale to square"),"Original scale")),
-					div(class="span6", sliderInput("image.theta","Rotate (Uncheck smoothing):",-180,180,0,10))
+						selectInput("image.asp", "Aspect ratio:", c("Original scale","Rescale to square"),"Original scale", width="100%")),
+					div(class="span6", sliderInput("image.theta","Rotate (Uncheck smoothing):",-180,180,0,10, width="100%"))
 				)
 			)
 		)
@@ -78,30 +78,30 @@ column(4,
 				div(class="row-fluid",
 					div(class="span6", 
 						selectInput("persp.col","Perspective colors:",
-							c("R:Topo","R:Terrain","R:Heat","R:Cyan-Magenta","black-white","purple-yellow"),"R:Topo")),
-					div(class="span6", sliderInput("adjustrelief","Scale relief:",-10,10,0,2))
+							c("R:Topo","R:Terrain","R:Heat","R:Cyan-Magenta","black-white","purple-yellow"),"R:Topo", width="100%")),
+					div(class="span6", sliderInput("adjustrelief","Scale relief:",-10,10,0,2, width="100%"))
 				),
 				div(class="row-fluid",
 					div(class="span6", 
-						selectInput("persp.contourside","2D contour level(s):", c("","Below","Layered","Above"), "", multiple=T)),
-					div(class="span6", sliderInput("persp.zlim1","Decrease lower Z-limit:",0,5,0,0.5))
+						selectInput("persp.contourside","2D contour level(s):", c("","Below","Layered","Above"), "", multiple=T, width="100%")),
+					div(class="span6", sliderInput("persp.zlim1","Decrease lower Z-limit:",0,5,0,0.5, width="100%"))
 				),
 				div(class="row-fluid",
-					div(class="span6", selectInput("persp.imageside","2D image level:",c("","Below","Above"),"",multiple=T)),
-					div(class="span6", sliderInput("persp.zlim2","Increase upper Z-limit:",0,5,0,0.5))
+					div(class="span6", selectInput("persp.imageside","2D image level:",c("","Below","Above"),"",multiple=T, width="100%")),
+					div(class="span6", sliderInput("persp.zlim2","Increase upper Z-limit:",0,5,0,0.5, width="100%"))
 				),
 				div(class="row-fluid",
 					div(class="span6", 
 						selectInput("bty", "Box and background grid:", 
 							c("Full box","Back panels only","Panels w/ grid lines","Gray w/ white lines",
-								"Black","Black w/ gray lines"),"Black w/ gray lines")),
-					div(class="span6", sliderInput("theta","Azimuthal viewing angle:",-180,180,40,10))
+								"Black","Black w/ gray lines"),"Black w/ gray lines", width="100%")),
+					div(class="span6", sliderInput("theta","Azimuthal viewing angle:",-180,180,40,10, width="100%"))
 				),
 				div(class="row-fluid",
 					div(class="span6", 
-						selectInput("along", "Ribbons along:", c("x-axis","y-axis","both axes"),"x-axis"), 
+						selectInput("along", "Ribbons along:", c("x-axis","y-axis","both axes"),"x-axis", width="100%"), 
 						checkboxInput("curtain","Drape edges")),
-					div(class="span6", sliderInput("phi","Colatitudinal viewing angle:",-90,90,40,10))
+					div(class="span6", sliderInput("phi","Colatitudinal viewing angle:",-90,90,40,10, width="100%"))
 				)
 			)
 		)
@@ -113,11 +113,11 @@ column(4,
 				div(class="row-fluid",
 					div(class="span6", 
 						selectInput("rglcol","RGL colors:",
-							c("R:Topo","R:Terrain","R:Heat","R:Cyan-Magenta","black-white","purple-yellow"),"R:Topo")),
+							c("R:Topo","R:Terrain","R:Heat","R:Cyan-Magenta","black-white","purple-yellow"),"R:Topo", width="100%")),
 					div(class="span6", 
-						selectInput("rglpointslines","Points/lines (if applicable):",c("Points","Lines"),"Points"))
+						selectInput("rglpointslines","Points/lines (if applicable):",c("Points","Lines"),"Points", width="100%"))
 				),
-				sliderInput("rgladjustrelief","Scale relief (percent):",-100,100,0,step=10)
+				sliderInput("rgladjustrelief","Scale relief (percent):",-100,100,0,step=10, width="100%")
 			)
 		)
 	),
@@ -127,13 +127,13 @@ column(4,
 			conditionalPanel(condition="input.showWP6",
 				div(class="row-fluid",
 					div(class="span6",
-						selectInput("colkey", "Color key:", c("None","Default","Manual"),"None"),
+						selectInput("colkey", "Color key:", c("None","Default","Manual"),"None", width="100%"),
 						checkboxInput("clab", "Show key label:"),
-						selectInput("NAcol", "NA color:", choices=c("white","black"), selected="white")),
+						selectInput("NAcol", "NA color:", choices=c("white","black"), selected="white", width="100%")),
 					div(class="span6",
-						selectInput("colkey.side","Color key position:", c("Bottom","Left","Top","Right"), "Right"), 
+						selectInput("colkey.side","Color key position:", c("Bottom","Left","Top","Right"), "Right", width="100%"), 
 						checkboxInput("colkey.addlines", "Add lines to key:"), 
-						selectInput("bgcol", "Background color:", c("Black","White"),"Black"))
+						selectInput("bgcol", "Background color:", c("Black","White"),"Black", width="100%"))
 				)
 			)
 		)
@@ -156,8 +156,8 @@ column(4,
 			widths=c(12,1)
 		),
 		div(class="row-fluid",
-			div(class="span6", selectInput("hltheme", "Code highlighting theme:", getAceThemes(), selected="clouds_midnight")),
-			div(class="span6", selectInput("hlfontsize", "Font size:", seq(8,24,by=2), selected=12))
+			div(class="span6", selectInput("hltheme", "Code highlighting theme:", getAceThemes(), selected="clouds_midnight", width="100%")),
+			div(class="span6", selectInput("hlfontsize", "Font size:", seq(8,24,by=2), selected=12, width="100%"))
 		),
 		h6(HTML(
 			'<p style="text-align:justify;">I use code externalization for more complex apps to keep the
