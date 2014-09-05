63e1d01a6a624cf615304c8b173c448c3d934286
leonawicz <matt.rstat@gmail.com>
2014-09-04 18:20:05 -0600

    File paths for local testing

--- cmip3_cmip5/global.R
+++ cmip3_cmip5/global.R
@@ -5,8 +5,10 @@ load("external/data_CRU31.RData", envir=.GlobalEnv)
 load("external/data_cities_CRU31.RData", envir=.GlobalEnv)
 
 #### If overriding these four objects from files_meta.RData ####
-region.gcm.files.path <- "Y:/Users/mfleonawicz/AR4_AR5_Comparisons/region_files_GCM"
-region.gcm.files <- list.files(region.gcm.files.path, full=T)
-city.gcm.files.path <- "Y:/Users/mfleonawicz/AR4_AR5_Comparisons/city_files_GCM"
-city.gcm.files <- list.files(city.gcm.files.path, full=T)
+if(Sys.info()["sysname"]=="Windows"){ # for local devl/testing
+	region.gcm.files.path <- "Y:/Users/mfleonawicz/AR4_AR5_Comparisons/region_files_GCM"
+	region.gcm.files <- list.files(region.gcm.files.path, full=T)
+	city.gcm.files.path <- "Y:/Users/mfleonawicz/AR4_AR5_Comparisons/city_files_GCM"
+	city.gcm.files <- list.files(city.gcm.files.path, full=T)
+}
 ###############################################################
