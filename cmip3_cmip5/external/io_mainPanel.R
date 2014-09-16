d37c3d890c5f5e50905f424588d5ed49a3f1d4c6
leonawicz <matt.rstat@gmail.com>
2014-09-15 18:11:01 -0600

    Description updates

--- cmip3_cmip5/external/io_mainPanel.R
+++ cmip3_cmip5/external/io_mainPanel.R
@@ -6,12 +6,12 @@ output$tsText <- renderUI({
 			<li>If months or decades left blank, selection defaults to all. Select only what you want, but no need to click every factor level.</li>
 			<li>Years are the intersection of the years slider and the decades list. One may suffice, unless you want partial or discontinuous decades.</li>
 			<li>You may subset data when minimally required selections are complete. This text will vanish and a blue subset button will appear.</li>
-			<li>You may download data when subset is complete.</li>
+			<li>You may download data when subset is complete. The data selection panel will minimize and the plot options panel will appear below.</li>
 		</ul>
 		<h3>Plot Options</h3>
 		<ul>
 			<li>Plot options are based on the data in use and must be selected after subsetting the data.</li>
-			<li>Since some plot options are based on the data subset, generating a new subset will generally require selecting some plot options again.</li>
+			<li>Since plot options are based on the data subset, any changes to data selections will require generating a new data set and selecting plot options again.</li>
 			<li>As with the data download, you may plot in the browser or download the plot as a PDF via the respective buttons when subsetting is complete.</li>
 		</ul>")
 })
@@ -24,12 +24,12 @@ output$spText <- renderUI({
 			<li>If months or decades left blank, selection defaults to all. Select only what you want, but no need to click every factor level.</li>
 			<li>Years are the intersection of the years slider and the decades list. One may suffice, unless you want partial or discontinuous decades.</li>
 			<li>You may subset data when minimally required selections are complete. This text will vanish and a blue subset button will appear.</li>
-			<li>You may download data when subset is complete.</li>
+			<li>You may download data when subset is complete. The data selection panel will minimize and the plot options panel will appear below.</li>
 		</ul>
 		<h3>Plot Options</h3>
 		<ul>
 			<li>Plot options are based on the data in use and must be selected after subsetting the data.</li>
-			<li>Since some plot options are based on the data subset, generating a new subset will generally require selecting some plot options again.</li>
+			<li>Since plot options are based on the data subset, any changes to data selections will require generating a new data set and selecting plot options again.</li>
 			<li>As with the data download, you may plot in the browser or download the plot as a PDF via the respective buttons when subsetting is complete.</li>
 		</ul>")
 })
@@ -42,12 +42,12 @@ output$varText <- renderUI({
 			<li>If months or decades left blank, selection defaults to all. Select only what you want, but no need to click every factor level.</li>
 			<li>Years are the intersection of the years slider and the decades list. One may suffice, unless you want partial or discontinuous decades.</li>
 			<li>You may subset data when minimally required selections are complete. This text will vanish and a blue subset button will appear.</li>
-			<li>You may download data when subset is complete.</li>
+			<li>You may download data when subset is complete. The data selection panel will minimize and the plot options panel will appear below.</li>
 		</ul>
 		<h3>Plot Options</h3>
 		<ul>
 			<li>Plot options are based on the data in use and must be selected after subsetting the data.</li>
-			<li>Since some plot options are based on the data subset, generating a new subset will generally require selecting some plot options again.</li>
+			<li>Since plot options are based on the data subset, any changes to data selections will require generating a new data set and selecting plot options again.</li>
 			<li>As with the data download, you may plot in the browser or download the plot as a PDF via the respective buttons when subsetting is complete.</li>
 		</ul>")
 })
