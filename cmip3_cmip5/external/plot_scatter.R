5c4b331296217a6bf049af45055398205fa64aa6
mfleonawicz <mfleonawicz@alaska.edu>
2014-09-17 11:04:20 -0800

    Improvements/fixes

---
+++ cmip3_cmip5/external/plot_scatter.R
@@ -0,0 +1,37 @@
+function(d, form.string, panels, grp, n.grp, facet.cols=ceiling(sqrt(panels)), facet.by, vert.facet=FALSE, fontsize=16,
+	colpal, colseq, contourlines=FALSE, hexbin=FALSE, pts.alpha=0.5, show.overlay=FALSE, overlay=NULL, jit=FALSE,
+	plot.title="", plot.subtitle="", show.panel.text=FALSE, show.title=FALSE, lgd.pos="Top", units=c("C","mm"),
+	mos=12, pooled.var, show.logo=F, logo.mat=NULL){
+		if(is.null(d)) return(plot(0,0,type="n",axes=F,xlab="",ylab=""))
+		if(show.overlay & !is.null(overlay)) show.overlay <- TRUE else show.overlay <- FALSE
+		if(show.overlay) overlay$Observed <- "CRU 3.1"
+		if(!length(lgd.pos)) lgd.pos="Top"
+		if(!length(fontsize)) fontsize <- 16
+		fontsize=as.numeric(fontsize)
+		if(is.null(pts.alpha)) pts.alpha <- 0.5
+		lab <- sp_xlabylab(units=units, form.string=form.string)
+		if(substr(lab$xlb, 1, 1)=="P") { x <- "Precipitation"; y <- "Temperature" } else { x <- "Temperature"; y <- "Precipitation" }
+		main <- paste0("temperature and precipitation: ", plot.title)
+		if(jit) point.pos <- position_jitter(0.1,0.1) else point.pos <- "identity"
+		grp <- adjustGroup(grp=grp, n.grp=n.grp)
+		if(grp==1) {colpal <- "none"; color <- fill <- NULL} else color <- fill <- grp
+		scfm <- scaleColFillMan_prep(fill=fill, col=colpal)
+		fill <- scfm$fill
+		if(length(vert.facet)) if(vert.facet) facet.cols <- 1
+		g <- ggplot(d, aes_string(x=x,y=y,group=grp,order=grp,colour=color,fill=fill)) + theme_bw(base_size=fontsize) + xlab(lab$xlb) + ylab(lab$ylb) + theme(legend.position=tolower(lgd.pos))
+		if(!show.logo && show.title) g <- g + ggtitle(bquote(atop(.(main))))
+		if(length(colpal) & length(colseq)) g <- scaleColFillMan(g=g, default=scfm$scfm, colseq=colseq, colpal=colpal, mos=mos, n.grp=n.grp, cbpalette=cbpalette) # cbpalette source?
+		if(!is.null(facet.by)) if(facet.by!="None/Force Pool") g <- g + facet_wrap(as.formula(paste("~",facet.by)), ncol=facet.cols)
+		g <- g + geom_point(position=point.pos, pch=21, size=4, colour="black", alpha=pts.alpha)
+		if(!is.null(contourlines) && contourlines) g <- g + stat_density2d(size=1)
+		if(hexbin) g <- g + scale_alpha(range=c(0.1,0.5), guide="none") + stat_binhex(bins=30, aes(alpha=..count..))
+		if(show.overlay){
+			observed.col <- if(grp==1) "red" else "black"
+			g <- g + geom_point(data=overlay, aes_string(x=x, y=y, colour=NULL, fill=NULL), position=point.pos, pch=21, size=4, fill="black", colour="red", alpha=pts.alpha)
+			if(!is.null(contourlines) && contourlines) g <- g + stat_density2d(data=overlay, aes_string(x=x, y=y, group=grp, colour=NULL, fill=NULL, size="Observed"), colour=observed.col) + guides(size=guide_legend(title="Observed"))
+			if(hexbin) g <- g + stat_binhex(data=overlay, aes_string(x=x, y=y, colour=NULL, fill=NULL, alpha="..count..", size="Observed"), colour=observed.col, bins=30)
+		}
+		g <- g + annotate("text", y=max(d[[y]]), x=min(d[[x]]), label=bquote(.(plot.subtitle)), hjust=0, vjust=1, fontface=3)
+		g <- addLogo(g, show.logo, logo.mat, show.title, main, fontsize)
+		print(g)
+}
