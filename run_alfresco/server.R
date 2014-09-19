4fe0d388b6a40712b3bdb10c438a368aca8f2e3a
mfleonawicz <mfleonawicz@alaska.edu>
2014-09-19 07:47:47 -0800

    RJSONIO toJSON methods arg fix

--- run_alfresco/server.R
+++ run_alfresco/server.R
@@ -9,7 +9,7 @@ slurmfile <- "RunAlfresco.slurm"
 setMethod("toJSON", "numeric",
    function(x, container = isContainer(x, asIs, .level), collapse = "\n", digits = 10, ...,
 	.level = 1L, .withNames = length(x) > 0 && length(names(x)) > 0,
-	.na = "null", .escapeEscapes = TRUE, pretty = FALSE, asIs = NA){
+	.na = "null", .escapeEscapes = TRUE, pretty = FALSE, asIs = NA, .inf = " Infinity"){
 
 	if(any(is.infinite(x))) warning("non-finite values in numeric vector may not be appropriately represented in JSON")
 	tmp = formatC(x, digits = digits)
