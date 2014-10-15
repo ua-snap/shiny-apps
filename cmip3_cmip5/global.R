5929d8460619f780c7ad6307e3fc1087017454a9
Matt Leonawicz <matt.rstat@gmail.com>
2014-10-15 11:55:39 -0600

    Metadata updates

--- cmip3_cmip5/global.R
+++ cmip3_cmip5/global.R
@@ -6,25 +6,28 @@ load("external/files_meta.RData",envir=.GlobalEnv)
 
 #### If overriding these four objects from files_meta.RData ####
 if(Sys.info()["sysname"]=="Windows"){ # for local devl/testing
-	region.gcm.stats.path <- list.files("Y:/Users/mfleonawicz/AR4_AR5_Comparisons/region_files_GCM/stats", full=T)
+	topDir <- "Y:" #"/workspace/Shared"
+	subDir <- "Users/mfleonawicz/AR4_AR5_Comparisons"
+	region.gcm.stats.path <- list.files(file.path(topDir, subDir, "region_files_GCM/stats"), full=T)
 	region.gcm.stats.files <- lapply(region.gcm.stats.path, list.files, full=T)
-	region.cru.stats.path <- list.files("Y:/Users/mfleonawicz/AR4_AR5_Comparisons/region_files_CRU/stats", full=T)
+	region.cru.stats.path <- list.files(file.path(topDir, subDir, "region_files_CRU/stats"), full=T)
 	region.cru.stats.files <- lapply(region.cru.stats.path, list.files, full=T)
 	
-	region.gcm.samples.path <- list.files("Y:/Users/mfleonawicz/AR4_AR5_Comparisons/region_files_GCM/samples", full=T)
-	# Hardcode for sidebar input
-	region.names <- c(names(region.names.out), "Cities")
+	region.gcm.samples.path <- list.files(file.path(topDir, subDir, "region_files_GCM/samples"), full=T)
 	region.gcm.samples.files <- lapply(region.gcm.samples.path, list.files, full=T)
-	region.cru.samples.path <- list.files("Y:/Users/mfleonawicz/AR4_AR5_Comparisons/region_files_CRU/samples", full=T)
+	region.cru.samples.path <- list.files(file.path(topDir, subDir, "region_files_CRU/samples"), full=T)
 	region.cru.samples.files <- lapply(region.cru.samples.path, list.files, full=T)
 	names(region.cru.stats.files) <- names(region.gcm.stats.files) <- names(region.cru.samples.files) <- names(region.gcm.samples.files) <- basename(region.gcm.stats.path)
 	
-	city.gcm.files.path <- "Y:/Users/mfleonawicz/AR4_AR5_Comparisons/city_files_GCM"
+	city.gcm.files.path <- file.path(topDir, subDir, "city_files_GCM")
 	city.gcm.files <- list.files(city.gcm.files.path, full=T)
-	city.cru.files.path <- "Y:/Users/mfleonawicz/AR4_AR5_Comparisons/city_files_CRU"
+	city.cru.files.path <- file.path(topDir, subDir, "city_files_CRU")
 	city.cru.files <- list.files(city.cru.files.path, full=T)
+	city.names <- gsub("--", ", ", basename(city.gcm.files))
 }
 ###############################################################
+
+region.names <- c(names(region.names.out), "Cities") # Hardcode for sidebar input
 
 help_tabpanel_conditional <- conditionalPanel(
 	condition=
