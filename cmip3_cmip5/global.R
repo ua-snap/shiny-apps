d5afacf5dc9c4ab21a327bb24a6b2278bfe95f7c
Matt Leonawicz <matt.rstat@gmail.com>
2015-02-02 19:08:11 -0700

    Upgrades and data updates

--- cmip3_cmip5/global.R
+++ cmip3_cmip5/global.R
@@ -1,20 +1,21 @@
-library(shinyIncubator)
-library(shinysky)
+library(shinythemes)
 library(markdown)
-load("external/meta.RData",envir=.GlobalEnv)
+print(paste("metadata .RData file load time:"))
+print(system.time( load("external/meta.RData",envir=.GlobalEnv) ))
 
 #### If overriding these region and city file path objects from files_meta.RData ####
+print(paste("Local file path swap (devel testing only):"))
+print(system.time({
 if(Sys.info()["sysname"]=="Windows"){ # for local devl/testing
-	topDir <- "Y:" #"/workspace/Shared"
-	subDir <- "Users/mfleonawicz/AR4_AR5_Comparisons"
-	region.gcm.stats.path <- list.files(file.path(topDir, subDir, "region_files_GCM/stats"), full=T)
+	topDir <- "C:/leonawicz/AR4_AR5_Comparisons"
+	region.gcm.stats.path <- list.files(file.path(topDir, "region_files_GCM/stats"), full=T)
 	region.gcm.stats.files <- lapply(region.gcm.stats.path, list.files, full=T)
-	region.cru.stats.path <- list.files(file.path(topDir, subDir, "region_files_CRU/stats"), full=T)
+	region.cru.stats.path <- list.files(file.path(topDir, "region_files_CRU/stats"), full=T)
 	region.cru.stats.files <- lapply(region.cru.stats.path, list.files, full=T)
 	
-	region.gcm.samples.path <- list.files(file.path(topDir, subDir, "region_files_GCM/samples"), full=T)
+	region.gcm.samples.path <- list.files(file.path(topDir, "region_files_GCM/samples"), full=T)
 	region.gcm.samples.files <- lapply(region.gcm.samples.path, list.files, full=T)
-	region.cru.samples.path <- list.files(file.path(topDir, subDir, "region_files_CRU/samples"), full=T)
+	region.cru.samples.path <- list.files(file.path(topDir, "region_files_CRU/samples"), full=T)
 	region.cru.samples.files <- lapply(region.cru.samples.path, list.files, full=T)
 	names(region.cru.stats.files) <- names(region.gcm.stats.files) <- names(region.cru.samples.files) <- names(region.gcm.samples.files) <- basename(region.gcm.stats.path)
 	
@@ -23,6 +24,7 @@ if(Sys.info()["sysname"]=="Windows"){ # for local devl/testing
 	city.gcm.files.path <- file.path(topDir, "city_files_GCM")
 	city.gcm.files <- gsub("city_files_CRU", "city_files_GCM", city.cru.files)
 }
+}))
 ###############################################################
 
 region.names <- c(names(region.names.out), "Cities") # Hardcode for sidebar input
