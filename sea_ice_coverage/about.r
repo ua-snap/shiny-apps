fe52d96b379329d729789f911e23b3e50fe2e80d
Matt Leonawicz <matt.rstat@gmail.com>
2015-02-04 09:39:40 -0700

    about.R updates

--- sea_ice_coverage/about.r
+++ sea_ice_coverage/about.r
@@ -5,18 +5,19 @@ function(){
 		<p style="text-align:justify">This web application shows sea ice coverage over the Arctic from 1860 and projected through 2099.
 		The first tab includes a time series plot of total sea ice extent by global climate model (GCM), including a five-model averaged composite. All GCMs are RCP 8.5.
 		Linear, quadratic, and locally weighted LOESS fitted trends are available for overlay.
-		Fitted trend model output is displayed below the plot for each climate model and regression model.</p>
+		Fitted trend model output is displayed below the plot for each climate model and regression model.
+		Keep in mind these are exploratory visualizations. They are not intended as valid regression models.</p>
 		
-		<p style="text-align:justify">The second tab shows a panel graphic of spatially explicit sea ice concentration values for each model including the composite.
+		<p style="text-align:justify">A second tab (developmental) shows a panel graphic of spatially explicit sea ice concentration values for each model including the composite.
 		<em>This graphic is for illustration purposes only</em> and should not be treated as spatially accurate with respect to the displayed land masses.
 		Pixels from the original raster datasets, aggregated to a coarser scale (for app efficiency),
-		are not intended to line up perfectly with the overlain landmass outlines, which are provided only for convenient reference and visual orientation for the user.
-		The sea ice concentration map tab is included in the app as an', em('extra feature,'),'primarily in the interest of general idea and code sharing with the broader R and Shiny user communities.
+		are not intended to line up perfectly with the overlaid landmass outlines, which are provided only for convenient reference and visual orientation for the user.
+		The sea ice concentration map tab is included in the app as an extra feature primarily in the interest of general idea and code sharing with the broader R and Shiny user communities.
 		For users of the app who are specifically interested in it for its sea ice content, it is recommended that you focus on the extent totals tab.</p>
 		
 		<p style="text-align:justify">The purpose of the app is to provide a relatively simple and convenient way of interactively exploring a subset of aggregated (across time and space) GCM sea ice outputs.
 		The goal is not to develop the perfect web tool, but rather to showcase some of the data.
-		As such, there are no plans for continued development or refinement of this particular app. However, future apps for other projects will be able to build upon what has been done here.</p>
+		As such, there are no plans for continued development or refinement of this particular app. However, future apps for other projects will be build upon what has been explored here.</p>
 		
 		<p style="text-align:justify"><em>This project is funded by the Alaska Ocean Observing System through its cooperative agreement ##NA11NOS0120020 with the National Oceanic and Atmospheric Administration (NOAA).
 		Work was performed by the Alaska Center for Climate Assessment and Policy(ACCAP) at the University of Alaska Fairbanks (UAF) in partnership with the Scenarios Network for Alaska and Arctic Planning (SNAP, UAF).</em></p>
