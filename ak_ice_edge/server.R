9878283a42e45f5fdab0e25a31e06815634af049
Matt Leonawicz <matt.rstat@gmail.com>
2015-02-12 12:24:25 -0700

    Code updates

--- ak_ice_edge/server.R
+++ ak_ice_edge/server.R
@@ -1,9 +1,4 @@
-library(shiny)
-library(raster)
-library(rasterVis)
-library(png)
-library(gridExtra)
-#library(RColorBrewer)
+lapply(list("shiny", "raster", "rasterVis", "png", "gridExtra", "RColorBrewer"), function(x) library(x, character.only=T))
 
 logo <- readPNG("www/img/SNAP_acronym_100px.png")
 logo.alpha <- 1
@@ -14,7 +9,7 @@ shinyServer(function(input, output, session){
 output$Compare <- renderUI({
 	vars <- c("Decade", "Month")
 	vars <- vars[-which(vars == input$color_by)]
-	selectInput("compare", "Secondary variable:", choices=c("", vars), selected="", width="100%")
+	selectInput("compare", "Secondary variable", choices=c("", vars), selected="", width="100%")
 })
 
 Decades <- reactive({
@@ -35,23 +30,13 @@ Months_Int <- reactive({
 	mos.int
 })
 
-Mos <- reactive({
-	c(paste0("0", 1:9), 10:12)[Months_Int()]
-})
+Mos <- reactive({ c(paste0("0", 1:9), 10:12)[Months_Int()] })
 
-Mos_Abb <- reactive({
-	month.abb[Months_Int()]
-})
+Mos_Abb <- reactive({ month.abb[Months_Int()] })
 
-ncols <- reactive({
-	if(input$color_by=="Decade") ncols <- length(Mos()) else ncols <- length(Decades())
-	ncols
-})
+ncols <- reactive({ if(input$color_by=="Decade") length(Mos()) else length(Decades()) })
 
-nrows <- reactive({
-	if(input$color_by=="Decade") nrows <- length(Decades()) else nrows <- length(Mos())
-	nrows
-})
+nrows <- reactive({ if(input$color_by=="Decade") length(Decades()) else length(Mos()) })
 
 SLDF_names <- reactive({
 	if(input$color_by=="Decade") {
@@ -73,9 +58,7 @@ SLDF_annual_names <- reactive({
 	x
 })
 
-Main_title <- reactive({
-	if(input$include_annual) "Annual and Mean Decadal 15% Sea Ice Concentration Edges" else "Mean Decadal 15% Sea Ice Concentration Edge"
-})
+Main_title <- reactive({ if(input$include_annual) "Annual and Mean Decadal 15% Sea Ice Concentration Edges" else "Mean Decadal 15% Sea Ice Concentration Edge" })
 
 Key_title <- reactive({
 	if(input$color_by=="Decade"){ # Designed for up to two columns in the plot key
@@ -88,25 +71,27 @@ Key_title <- reactive({
 	keytitle
 })
 
-Key_text <- reactive({
-	if(input$color_by=="Decade"){ # Designed for up to two columns in the plot key
-		keytext <- list(rep(Decades(),ncols()))
-	} else if(input$color_by=="Month") {
-		keytext <- list(rep(Mos_Abb(),ncols()))
-	}
-	keytext
-})
+Key_text <- reactive({ if(input$color_by=="Decade") list(rep(Decades(), ncols())) else list(rep(Mos_Abb(), ncols())) })
 
-#clrs <- rev(brewer.pal(11, "Spectral")[c(1:3, 9:11)]) # Number of colors must equal number of elements in 'input$color_by' variable
+Colors <- reactive({
+	n <- nrows()
+	colpal <- strsplit(input$colpal, " \\[")[[1]][1]
+	ind <- which(rownames(brewer.pal.info)==colpal)
+	maxcol <- brewer.pal.info$maxcolors[ind]
+	type <- brewer.pal.info$category[ind]
+	x <- if(type=="qual") rep(rev(brewer.pal(maxcol, colpal)), length=n) else colorRampPalette(rev(brewer.pal(maxcol, colpal)))(n+1)
+	if(type=="div") x <- x[-round(median(1:n))] else if(type=="seq")  x <- x[-(n+1)]
+	x
+})
 
 doPlot_plotMap <- function(...){
 	if(!(is.na(Mos()[1]) | !length(Decades()) | is.null(nrows()) | is.null(ncols()) | is.null(Key_text()) | is.null(Key_title()) | is.null(SLDF_names()) | is.null(input$include_annual))){
-		plotMap(r=r3, nr=nrows(), nc=ncols(), key.text=Key_text(), key.title=Key_title(), sldf.names=SLDF_names(), sldf.annual.names=SLDF_annual_names(), annual=input$include_annual, main.title=Main_title(), ...)
+		plotMap(r=r3, nr=nrows(), nc=ncols(), key.text=Key_text(), key.title=Key_title(), sldf.names=SLDF_names(), sldf.annual.names=SLDF_annual_names(), annual=input$include_annual, clrs=Colors(), main.title=Main_title(), ...)
 	} else NULL
 }
 
 # Currently allowing download as PNG only
-plotMap <- function(r, nr, nc, key.text, key.title, sldf.names, sldf.annual.names, annual=FALSE, main.title="Plot", PDF=FALSE, PNG=FALSE, filename=NULL, show.logo=F, logo.mat=NULL, verbose=TRUE){
+plotMap <- function(r, nr, nc, key.text, key.title, sldf.names, sldf.annual.names, annual=FALSE, clrs, main.title="Plot", PDF=FALSE, PNG=FALSE, filename=NULL, show.logo=F, logo.mat=NULL, verbose=TRUE){
 	if(verbose){
 		prog.max <- 2+2*nr+1+3+3
 		progress <- Progress$new(session, min=1, max=prog.max)
@@ -122,10 +107,10 @@ plotMap <- function(r, nr, nc, key.text, key.title, sldf.names, sldf.annual.name
 		main=list(label=main.title, cex=maincex),
 		sub=list(label=expression(italic("Edges are estimated 15% contour lines for mean decadal sea ice concentration.")), cex=maincex),
 		panel=panel.levelplot.raster,
-		par.settings=modifyList(theme1, list( superpose.symbol=list(col=clrs[1:nr], lty=1, lwd=1, fill="white"), layout.heights=list(top.padding=pad, bottom.padding=pad/1.25) )),
+		par.settings=modifyList(theme1, list( superpose.symbol=list(col=clrs, lty=1, lwd=1, fill="white"), layout.heights=list(top.padding=pad, bottom.padding=pad/1.25) )),
 		key=list(
 			x=1-0.05*(nrow(r)/ncol(r)), y=0.05, corner=c(1, 0), size=3,
-			lines=list(lty=rep(c(1,2),each=nr)[1:(nr*nc)], lwd=2, col=clrs[1:nr]),
+			lines=list(lty=rep(c(1,2),each=nr)[1:(nr*nc)], lwd=2, col=clrs),
 			text=key.text,
 			fontfamily=font.fam,
 			background="white", border="black", padding.text=2,
@@ -137,16 +122,16 @@ plotMap <- function(r, nr, nc, key.text, key.title, sldf.names, sldf.annual.name
 		for(i in 1:nr){ # Add annual lines
 			if(verbose)progress$set(message="Adding annual ice edges", detail=paste("decade(s)", i, "of", nr, "..."), value=2+i)
 			#Sys.sleep(0.2)
-			p <- p + layer(sp.lines(x, col=clrs[i], lwd=1, alpha=0.5), data=list(x=get(sldf.annual.names[i]), i=i))
-			if(nc>1) p <- p + layer(sp.lines(x, col=clrs[i], lwd=1, lty=5, alpha=0.5), data=list(x=get(sldf.annual.names[i+nr]), i=i))
+			p <- p + layer(sp.lines(x, col=clrs[i], lwd=1, alpha=0.5), data=list(x=get(sldf.annual.names[i]), clrs=clrs, i=i))
+			if(nc>1) p <- p + layer(sp.lines(x, col=clrs[i], lwd=1, lty=5, alpha=0.5), data=list(x=get(sldf.annual.names[i+nr]), clrs=clrs, i=i))
 		}
 	}
 	if(verbose) progress$set(message="Generating plot, please wait", detail="Adding layers: ice edges...", value=2+nr+1)
 	for(i in 1:nr){ # Add decadal lines
 		if(verbose) progress$set(message="Adding decadal ice edges", detail=paste("decade(s)", i, "of", nr, "..."), value=2+nr+1+i)
 		#Sys.sleep(0.2)
-		p <- p + layer(sp.lines(x, col=clrs[i], lwd=2), data=list(x=get(sldf.names[i]), i=i))
-		if(nc>1) p <- p + layer(sp.lines(x, col=clrs[i], lwd=2, lty=5), data=list(x=get(sldf.names[i+nr]), i=i))
+		p <- p + layer(sp.lines(x, col=clrs[i], lwd=2), data=list(x=get(sldf.names[i]), clrs=clrs, i=i))
+		if(nc>1) p <- p + layer(sp.lines(x, col=clrs[i], lwd=2, lty=5), data=list(x=get(sldf.names[i+nr]), clrs=clrs, i=i))
 	}
 	# Overlay "background"
 	for(i in 1:3){
@@ -177,10 +162,10 @@ output$PlotMap <- renderPlot({
 	isolate({ doPlot_plotMap() })
 	}, height=function(){ w <- session$clientData$output_PlotMap_width; if(length(w)) return(round(0.8*w)) else return("auto") }, width="auto")
 
-output$dlCurPlot1_PDF <- downloadHandler(
-	filename='Estimated_15pct_SIC_Edges_Map.pdf',
-	content=function(file){ pdf(file=file, width=1*10, height=1*8, pointsize=12, onefile=FALSE); doPlot_plotMap(PDF=T, show.logo=T, logo.mat=logo.mat, verbose=F); dev.off() }
-)
+#output$dlCurPlot1_PDF <- downloadHandler(
+#	filename='Estimated_15pct_SIC_Edges_Map.pdf',
+#	content=function(file){ pdf(file=file, width=1*10, height=1*8, pointsize=12, onefile=FALSE); doPlot_plotMap(PDF=T, show.logo=T, logo.mat=logo.mat, verbose=F); dev.off() }
+#)
 
 output$dlCurPlot1_PNG <- downloadHandler(
 	filename = 'Estimated_15pct_SIC_Edges_Map.png',
--- ak_ice_edge/ui.R
+++ ak_ice_edge/ui.R
@@ -1,22 +1,47 @@
 library(shinythemes)
 
-tabPanelAbout <- source("external/about.R",local=T)$value
-
-headerPanel_2 <- function(title, h, windowTitle=title) {    
-  tagList(
-    tags$head(tags$title(windowTitle)),
-      h(title)
-    )
-}
-
 shinyUI(fluidPage(theme=shinytheme("cosmo"),
-	headerPanel_2(
-		HTML('Estimated Mean Decadal 15% Sea Ice Concentration Edges
-			<a href="http://snap.uaf.edu" target="_blank"><img id="stats_logo" align="right" alt="SNAP Logo" src="./img/SNAP_acronym_100px.png" /></a>'
-		), h3, "Sea Ice Edge"
+	progressInit(),
+	tags$head(tags$link(rel="stylesheet", type="text/css", href="styles.css")),
+	navbarPage(
+		title=HTML('<div><a href="http://snap.uaf.edu" target="_blank"><img src="./img/SNAP_acronym_100px.png" width="80%"></a></div>'),
+		tabPanel("Map", value="map"),
+		tabPanel("About", value="about"),
+		windowTitle="Sea Ice Edge",
+		collapsible=TRUE,
+		id="tsp"
 	),
+	h4("Estimated Mean Decadal 15% Sea Ice Concentration Edges"),
+	conditionalPanel("input.tsp=='map'",
 	fluidRow(
-		source("external/sidebar.R",local=T)$value,
-		source("external/main.R",local=T)$value
+		column(4,
+			wellPanel(
+				selectInput("months_abb", "Months", month.abb, selected="", multiple=T),
+				selectInput("decades", "Decades", decades.all.lab, selected=decades.all.lab, multiple=T),
+				fluidRow(
+					column(6, selectInput("color_by", "Primary variable", c("Decade", "Month"), selected="Decade")),
+					column(6, uiOutput("Compare"))
+				),
+				fluidRow(
+					column(6, selectInput("colpal", "Colors", paste0(rownames(brewer.pal.info), " [",brewer.pal.info$category, "]"), selected="RdYlBu [div]")),
+					column(6, checkboxInput("include_annual", "Include annual edges", FALSE))
+				),
+				conditionalPanel(condition="input.months_abb !== null && input.decades !== null",
+					fluidRow(
+						column(6, actionButton("plot_button", "Generate Plot", icon=icon("check"), class="btn-block btn-primary")),
+						column(6, downloadButton("dlCurPlot1_PNG", "Get Plot", class="btn-block btn-success"))
+					)
+				)
+			),
+			h6(HTML(
+				'<p style="text-align:justify;">Select at least one month and decade. Primary variable: colored by level.
+				Secondary variable: Solid vs. dashed lines, two levels permitted, truncated to first two levels if more supplied.
+				If not selected, truncated to one level if more supplied.</p>
+				<p style="text-align:justify;">See the <code>About</code> tab above for more details.</p>'
+			))
+		),
+		column(8, plotOutput("PlotMap", width="100%", height="auto"))
 	)
+	),
+	conditionalPanel("input.tsp=='about'", source("about.R",local=T)$value)
 ))
