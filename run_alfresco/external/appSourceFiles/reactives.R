3146104ba145aa9b67bf4bdbdc827f47e0af7707
Matt Leonawicz <matt.rstat@gmail.com>
2015-04-09 16:43:30 -0600

    Unique run directories

--- run_alfresco/external/appSourceFiles/reactives.R
+++ run_alfresco/external/appSourceFiles/reactives.R
@@ -144,7 +144,7 @@ Obs_updateFiles <- reactive({
 			domainDir <- paste0("Runs_", alf.domain)
 			userDir <- gsub("@", "_at_", user_email_address())
 			#outDir <- paste0(mainDir,"/",domainDir,"/",userDir,"/",format(Sys.time(), "%Y-%m-%d-%H-%M-%S"))
-			outDir <- paste0(mainDir,"/",domainDir,"/",userDir)
+			outDir <- file.path(mainDir, domainDir, userDir, gsub("[ -]", "", input$run_name))
 			relDir <- outDir # Still need this?
 			
 			for(i in JSON_current()){
