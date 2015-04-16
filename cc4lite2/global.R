eb85f5f07ba87dd5ba24b7175d24f1a1fe3466cc
Matt Leonawicz <matt.rstat@gmail.com>
2015-04-16 12:35:29 -0600

    Split workspaces and move location commands

--- cc4lite2/global.R
+++ cc4lite2/global.R
@@ -1,6 +1,6 @@
 lapply(list("shiny", "shinythemes", "shinyBS", "rCharts", "plyr"), function(x) library(x, character.only=T))
-load(list.files(pattern="\\.RData$"))
+lapply(list.files(pattern="^cc4lite_launch_.*.\\.RData$"), load, envir=.GlobalEnv)
 caption <- 'Due to variability among climate models and among years in a natural climate system, these graphs are useful for examining trends over time, rather than for precisely<br>predicting monthly or yearly values. For more information on derivation, reliability, and variability among these projections, please visit www.snap.uaf.edu.'
 dec.lab <- paste0(seq(2010, 2090, by=10), "s")
-prov <- sapply(strsplit(locs.10min, ", "), tail, n=1) # move these two lines to .RData file
-locs <- tapply(locs.10min, prov, c, simplify=F)
+#prov <- sapply(strsplit(locs.10min, ", "), tail, n=1) # move these two lines to .RData file
+#locs <- tapply(locs.10min, prov, c, simplify=F)
