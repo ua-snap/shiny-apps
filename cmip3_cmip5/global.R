32cdf1ed7fd0834d1c3233ca3d46413b0a75064b
Matt Leonawicz <matt.rstat@gmail.com>
2015-04-17 16:37:55 -0600

    Cleanup and update

--- cmip3_cmip5/global.R
+++ cmip3_cmip5/global.R
@@ -1,31 +1,30 @@
 library(shinythemes)
 library(shinyIncubator)
 library(markdown)
-print(paste("metadata .RData file load time:"))
-print(system.time( load("meta.RData",envir=.GlobalEnv) ))
+load("meta.RData",envir=.GlobalEnv)
 
 #### If overriding these region and city file path objects from files_meta.RData ####
-print(paste("Local file path swap (devel testing only):"))
-print(system.time({
 if(Sys.info()["sysname"]=="Windows"){ # for local devl/testing
+	print(paste("Local file path swap (devel testing only)"))
 	topDir <- "C:/leonawicz/AR4_AR5_Comparisons"
 	region.gcm.stats.path <- list.files(file.path(topDir, "region_files_GCM/stats"), full=T)
 	region.gcm.stats.files <- lapply(region.gcm.stats.path, list.files, full=T)
-	region.cru.stats.path <- list.files(file.path(topDir, "region_files_CRU/stats"), full=T)
+	region.cru.stats.path <- list.files(file.path(topDir, "region_files_CRU32/stats"), full=T)
 	region.cru.stats.files <- lapply(region.cru.stats.path, list.files, full=T)
 	
 	region.gcm.samples.path <- list.files(file.path(topDir, "region_files_GCM/samples"), full=T)
 	region.gcm.samples.files <- lapply(region.gcm.samples.path, list.files, full=T)
-	region.cru.samples.path <- list.files(file.path(topDir, "region_files_CRU/samples"), full=T)
+	region.cru.samples.path <- list.files(file.path(topDir, "region_files_CRU32/samples"), full=T)
 	region.cru.samples.files <- lapply(region.cru.samples.path, list.files, full=T)
 	names(region.cru.stats.files) <- names(region.gcm.stats.files) <- names(region.cru.samples.files) <- names(region.gcm.samples.files) <- basename(region.gcm.stats.path)
 	
-	city.cru.files.path <- file.path(topDir, "city_files_CRU32")
-	city.cru.files <- list.files(city.cru.files.path, full=T)
 	city.gcm.files.path <- file.path(topDir, "city_files_GCM")
-	city.gcm.files <- gsub("city_files_CRU32", "city_files_GCM", city.cru.files)
+	city.gcm.files.2km <- list.files(file.path(city.gcm.files.path, "akcan2km"), full=T)
+	city.gcm.files.10min <- list.files(file.path(city.gcm.files.path, "world10min"), full=T)
+	city.cru.files.path <- file.path(topDir, "city_files_CRU32")
+	city.cru.files.2km <- list.files(file.path(city.cru.files.path, "akcan2km"), full=T)
+	city.cru.files.10min <- list.files(file.path(city.cru.files.path, "world10min"), full=T)
 }
-}))
 ###############################################################
 
 region.names <- c(names(region.names.out), "Cities") # Hardcode for sidebar input
