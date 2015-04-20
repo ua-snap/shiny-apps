44c121fe44b7f706214e5c6f8ac86b049194df64
Matt Leonawicz <matt.rstat@gmail.com>
2015-04-20 14:24:02 -0600

    Remove progressInit()

--- cmip3_cmip5/ui.R
+++ cmip3_cmip5/ui.R
@@ -10,7 +10,6 @@ headerPanel_2 <- function(title, h, windowTitle=title) {
 #library(ShinyDash)
 
 shinyUI(fluidPage(theme=shinytheme("cosmo"),
-	progressInit(),
 	#headerPanel_2(
 	#	HTML('Alaska and western Canada downscaled CMIP3/CMIP5 GCM comparison
 	#		<a href="http://snap.uaf.edu" target="_blank"><img id="stats_logo" align="right" alt="SNAP Logo" src="./img/SNAP_acronym_100px.png" /></a>'
