238b619a445a41d668cb1b6d478c520a08a88e4b
Matt Leonawicz <matt.rstat@gmail.com>
2015-04-20 14:22:32 -0600

    Improve plot dimensions reactivity

--- cmip3_cmip5/external/main.R
+++ cmip3_cmip5/external/main.R
@@ -1,27 +1,27 @@
 column(8,
 	help_tabpanel_conditional,
 	conditionalPanel(condition="input.tsp == 'plot_ts' && input.goButton !== null && input.goButton > 0",
-		plotOutput("PlotTS",height="auto"),
+		plotOutput("PlotTS", width="100%", height="auto"),
 		uiOutput("tsTextSub"),
 		uiOutput("TableTS")
 	),
 	conditionalPanel(condition="input.tsp == 'plot_scatter' && input.goButton !== null && input.goButton > 0",
-		plotOutput("PlotScatter",height="auto"),
+		plotOutput("PlotScatter", width="100%", height="auto"),
 		uiOutput("spTextSub"),
 		uiOutput("TableScatter")
 	),
 	conditionalPanel(condition="input.tsp == 'plot_heatmap' && input.goButton !== null && input.goButton > 0",
-		plotOutput("PlotHeatmap",height="auto"),
+		plotOutput("PlotHeatmap", width="100%", height="auto"),
 		uiOutput("hmTextSub"),
 		uiOutput("TableHeatmap")
 	),
 	conditionalPanel(condition="input.tsp == 'plot_variability' && input.goButton !== null && input.goButton > 0",
-		plotOutput("PlotVariability",height="auto"),
+		plotOutput("PlotVariability", width="100%", height="auto"),
 		uiOutput("varTextSub"),
 		uiOutput("TableVariability")
 	),
 	conditionalPanel(condition="input.tsp == 'plot_spatial' && input.goButton !== null && input.goButton > 0",
-		plotOutput("PlotSpatial",height="auto"),
+		plotOutput("PlotSpatial", width="100%", height="auto"),
 		uiOutput("spatialTextSub"),
 		uiOutput("TableSpatial")
 	)
