2209692044bed6420be60588e107dab31f38750e
Matt Leonawicz <matt.rstat@gmail.com>
2015-05-21 18:14:03 -0600

    Update text, load leaflet

--- cc4liteFinal/global.R
+++ cc4liteFinal/global.R
@@ -1,4 +1,10 @@
-lapply(list("shiny", "shinythemes", "shinyBS", "rCharts", "plyr"), function(x) library(x, character.only=T))
+lapply(list("shiny", "shinythemes", "shinyBS", "rCharts", "plyr", "leaflet"), function(x) library(x, character.only=T))
 lapply(list.files(pattern="^cc4lite_launch_.*.\\.RData$"), load, envir=.GlobalEnv)
-caption <- 'Due to variability among climate models and among years in a natural climate system, these graphs are useful for examining trends over time, rather than for precisely<br>predicting monthly or yearly values. For more information on derivation, reliability, and variability among these projections, please visit www.snap.uaf.edu.'
+caption <- 'Due to inter-annual variability and model uncertainty, these graphs are useful for examining a range of projected trends, but not for precise prediction. For more information regarding climate projections, please visit'
 dec.lab <- paste0(seq(2010, 2090, by=10), "s")
+
+brks <- c(0, 1e4, 5e4, 1e5, 2.5e5, 5e5, 1e6)
+nb <- length(brks)
+cities.meta$PopClass <- cut(cities.meta$Population, breaks=brks, include.lowest=TRUE, labels=FALSE)
+cities.meta$PopClass[is.na(cities.meta$PopClass)] <- 1
+palfun <- colorFactor(palette=c("navy", "navy", "magenta4", "magenta4", "red", "red"), domain=1:(nb-1))
