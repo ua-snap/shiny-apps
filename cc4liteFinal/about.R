e5d5634ad29772d71ae362c1f623951e9996ab0b
Matt Leonawicz <matt.rstat@gmail.com>
2015-05-27 10:12:08 -0600

    Text updates

--- cc4liteFinal/about.R
+++ cc4liteFinal/about.R
@@ -1,9 +1,10 @@
 tabPanel("About",
 	HTML(
-	'<p style="text-align:justify">This R Shiny web application presents climate outlooks for various Alaska and western Canada communities.
+	'<p style="text-align:justify">This R Shiny web application presents future climate outlooks for various Alaska and western Canada communities.
 	The type of plot shown is a range bar, or column range, plot. It shows the range of values (minimum to maximum) among SNAP\'s five downscaled CMIP5/AR5 climate models.
 	This highlights uncertainty about the future rather than focusing on an average, and without having to clutter the plot with additional ink.
-	This is the final of several versions of the Community Charts application. See the <a href="http://leonawicz.github.io/CommCharts4/">CommCharts4</a> documentation for more information.</p>'),
+	A range is shown similarly for the 30-year historical baseline comparison period, which is inclusive of inter-annual variability, but does not include other sources of uncertainty.
+	This is the final of several versions of the R Shiny-based Community Charts application. See the <a href="http://leonawicz.github.io/CommCharts4/">CommCharts4</a> documentation for more information.</p>'),
 		
 	HTML('
 	<div style="clear: left;"><img src="http://www.gravatar.com/avatar/52c27b8719a7543b4b343775183122ea.png" alt="" style="float: left; margin-right:5px" /></div>
@@ -28,6 +29,7 @@ tabPanel("About",
 				<li><a href="https://github.com/ebailey78/shinyBS" target="_blank">shinyBS</a></li>
 				<li><a href="http://rcharts.io/" target="_blank">rCharts</a></li>
 				<li><a href="http://plyr.had.co.nz/" target="_blank">plyr</a></li>
+				<li><a href="http://rstudio.github.io/leaflet/" target="_blank">plyr</a></li>
 				</ul>
 				<li>Source code on <a href="https://github.com/ua-snap/shiny-apps/tree/master/ak_ice_edge/" target="_blank">GitHub</a></li>
 			</ul>')
