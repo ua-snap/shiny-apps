880d8fa85e1b7c65566eff076c1a1d4c2143af41
Matt Leonawicz <matt.rstat@gmail.com>
2015-06-19 16:48:35 -0600

    Bug fix and simplify options

--- sea_ice_winds/external/main.R
+++ sea_ice_winds/external/main.R
@@ -4,10 +4,8 @@ column(8,
 			plotOutput("plotByYear", width="100%", height="auto"),
 			plotOutput("plotByDecade", width="100%", height="auto"), br(), br(),
 			fluidRow(
-				column(3, uiOutput("tp.annstyle")),
-				column(3, uiOutput("tp.decstyle")),
-				column(3, downloadButton("dl_plotByYear","Annual graphic", class="btn-block btn-primary"), br()),
-				column(3, downloadButton("dl_plotByDecade","Decadal graphic", class="btn-block btn-primary"))
+				column(6, downloadButton("dl_plotByYear","Annual graphic", class="btn-block btn-primary"), br()),
+				column(6, downloadButton("dl_plotByDecade","Decadal graphic", class="btn-block btn-primary"))
 			),
 			value="ts"),
 		tabPanelAbout(),
