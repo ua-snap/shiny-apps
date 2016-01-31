57a6d26a8b0f29f5e6b47a16b3f4cacf57b80daf
Matt Leonawicz <matt.rstat@gmail.com>
2016-01-31 12:42:31 -0700

    Remove files

--- us_daily_climate/external/appSourceFiles/io.mainPanel.tp1.R
+++
@@ -1,7 +0,0 @@
-# TabPanel titles
-output$tp.dailyTitle <- renderUI({
-	input$genPlotButton
-	isolate({
-		if(is.null(d())) h4("New plots may take several seconds to generate") else h4(paste(input$loc,"daily precipitation"))
-	})
-})
