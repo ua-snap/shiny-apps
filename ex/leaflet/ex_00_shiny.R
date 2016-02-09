5f634c8e28b8fdd3c064c7c72e2497e304f14e92
Matt Leonawicz <matt.rstat@gmail.com>
2016-02-08 18:55:11 -0700

    testing

--- ex/leaflet/ex_00_shiny.R
+++ ex/leaflet/ex_00_shiny.R
@@ -1,7 +1,7 @@
 library(shiny)
 library(dplyr)
 library(leaflet)
-stop(paste("Current working directory is:" getwd(), "; should be: /var/www/shiny-server/shiny-apps/ex/leaflet/"))
+stop(paste("Current working directory is:", getwd(), "; should be: /var/www/shiny-server/shiny-apps/ex/leaflet/"))
 load("nwt_locations.RData")
 
 lon <- -119.25
