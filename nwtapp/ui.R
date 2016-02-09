80f309fe61c72b3b51f2a7ea2ea60a45d708148b
Matt Leonawicz <matt.rstat@gmail.com>
2016-02-08 22:55:19 -0700

    Updates

--- nwtapp/ui.R
+++ nwtapp/ui.R
@@ -1,6 +1,6 @@
 source("about.R", local=T)
 
-shinyUI(navbarPage(theme=shinytheme("spacelab"), inverse=TRUE,
+shinyUI(navbarPage(theme="http://bootswatch.com/spacelab/bootstrap.css", inverse=TRUE,
   #tags$style(type="text/css", "html, body {width:100%;height:100%}"),
   title=HTML('<div><a href="http://snap.uaf.edu" target="_blank"><img src="./img/SNAP_acronym_100px.png" width="100%"></a></div>'),
   windowTitle="NT Climate",
