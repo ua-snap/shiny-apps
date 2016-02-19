7d9e8233611f145c30317f3b1f9593c7baee783c
Matt Leonawicz <matt.rstat@gmail.com>
2016-02-19 16:19:26 -0700

    Updates

--- nwtapp/global.R
+++ nwtapp/global.R
@@ -1,5 +1,18 @@
-pkgs <- list("shiny", "shinyBS", "shinyjs", "leaflet", "rgdal", "raster", "data.table", "dplyr", "tidyr", "ggplot2")
-suppressMessages(lapply(pkgs, function(x) library(x, character.only=T)))
+#pkgs <- list("shiny", "shinyBS", "shinyjs", "leaflet", "rgdal", "raster", "data.table", "dplyr", "tidyr", "ggplot2")
+#suppressMessages(lapply(pkgs, function(x) library(x, character.only=T)))
+
+suppressMessages({
+library(shiny)
+library(shinyBS)
+library(shinyjs)
+library(leaflet)
+library(rgdal)
+library(raster)
+library(data.table)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+})
 
 options(warn = -1) # # haven't figured out how to suppress warning from colors(.)
 
@@ -30,3 +43,5 @@ colpals <- RColorBrewer::brewer.pal.info
 dv <- rownames(colpals)[colpals["category"]=="div"]
 sq <- rownames(colpals)[colpals["category"]=="seq"]
 colpals_list <- list(Divergent=c(Custom="Custom div", dv), Sequential=c(Custom="Custom seq", sq))
+
+theme1 <- theme(plot.background=element_blank(), legend.position="bottom")
