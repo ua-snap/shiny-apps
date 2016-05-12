cd7b2f7d12152de8422e61cceb69cbf1d3440b52
Matt Leonawicz <matt.rstat@gmail.com>
2016-05-11 20:02:40 -0600

    Update app

--- plot3D/ga-plot3D.js
+++ plot3D/ga-plot3D.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-46129458-5', {'name':'all'});
-ga('all.send', 'pageview');
+ga('create', 'UA-46129458-9', 'auto');
+ga('send', 'pageview');
