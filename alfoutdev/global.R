a676aaa26c074359018be48ccef7b9b0bb6e37b3
leonawicz <matt.rstat@gmail.com>
2017-08-18 15:21:57 -0600

    Adjust global objects.

--- alfoutdev/global.R
+++ alfoutdev/global.R
@@ -1,4 +1,5 @@
 library(shinythemes)
-wsfiles <- list.files(pattern=".*.AB_FRP.*.RData$", full=T)
+wsfiles <- list.files(pattern=".*.AB_FRP.*.RData$", full=TRUE)
 load(wsfiles[1], envir=.GlobalEnv)
+rm(d.fs, fri.dat, frp.dat, rab.dat)
 vegclasses <- c("Alpine", "Forest", "Shrub", "Graminoid", "Wetland")
